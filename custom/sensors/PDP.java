daedc6a552d54f993a571390cf68a1a7847e6f10
Carter Turnbaugh <carterturn@tutanota.de>
2015-08-14 01:03:30 -0700

    Added PDP and LimitSwitchSystem (probably common enough)

---
+++ custom/sensors/PDP.java
@@ -0,0 +1,8 @@
+package org.usfirst.frc4904.cmdbased.custom.sensors;
+
+
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+
+public class PDP extends PowerDistributionPanel {
+	public PDP() {}
+}
