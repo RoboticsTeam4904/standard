9b7fe518fdf13a047591d49bf260bdd1a015f4fd
Carter Turnbaugh <carter@localhost.localdomain>
2015-08-29 14:27:55 -0700

    Made EncodedMotor take a Motor, not a SpeedController
    This lets us seperate the Motor subsystem from the PID loop,
    which will be helpful for overrides.

--- subsystems/motor/EncodedMotor.java
+++ subsystems/motor/EncodedMotor.java
@@ -1,18 +1,17 @@
 package org.usfirst.frc4904.cmdbased.subsystems.motor;
 
 
-import org.usfirst.frc4904.cmdbased.OutPipable;
 import org.usfirst.frc4904.logkitten.LogKitten;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
-public class EncodedMotor extends PIDSubsystem implements SpeedController, OutPipable {
-	protected final SpeedController motor;
+public class EncodedMotor extends PIDSubsystem implements SpeedController {
+	protected final Motor motor;
 	protected final Encoder encoder;
 	private final LogKitten logger;
 	
-	public EncodedMotor(String name, double P, double I, double D, SpeedController motor, Encoder encoder) {
+	public EncodedMotor(String name, double P, double I, double D, Motor motor, Encoder encoder) {
 		super(name, P, I, D);
 		this.motor = motor;
 		logger = new LogKitten(LogKitten.LEVEL_DEBUG, LogKitten.LEVEL_DEBUG);
@@ -47,27 +46,14 @@ public class EncodedMotor extends PIDSubsystem implements SpeedController, OutPi
 	public void set(double speed, byte arg1) {
 		set(speed);
 	}
-	private EncodedMotorMode pipeMode;
 	
-	public enum EncodedMotorMode {
-		PIDSET, SET;
-	}
-	
-	public void writePipe(double[] data) {
-		switch (pipeMode) {
-			case PIDSET:
-				set(data[0]);
-				return;
-			case SET:
-				motor.set(data[0]);
-				return;
-			default:
-				set(data[0]);
-				return;
-		}
-	}
-	
-	public void setPipe(Enum mode) {
-		pipeMode = (EncodedMotorMode) mode;
+	/**
+	 * This allows you to write to the motor directly.
+	 * For overrides (you terrible person)
+	 * 
+	 * @return the motor of the EncodedMotor
+	 */
+	public Motor getMotor() {
+		return motor;
 	}
 }
