03b2b184f98f812dfa44e05bd243eb13d8881909
Carter Turnbaugh <carter@localhost.localdomain>
2015-08-29 14:22:52 -0700

    Added pipe to EncodedMotor

--- subsystems/motor/EncodedMotor.java
+++ subsystems/motor/EncodedMotor.java
@@ -1,17 +1,21 @@
 package org.usfirst.frc4904.cmdbased.subsystems.motor;
 
 
+import org.usfirst.frc4904.cmdbased.OutPipable;
+import org.usfirst.frc4904.logkitten.LogKitten;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
-public class EncodedMotor extends PIDSubsystem implements SpeedController {
+public class EncodedMotor extends PIDSubsystem implements SpeedController, OutPipable {
 	protected final SpeedController motor;
 	protected final Encoder encoder;
+	private final LogKitten logger;
 	
 	public EncodedMotor(String name, double P, double I, double D, SpeedController motor, Encoder encoder) {
 		super(name, P, I, D);
 		this.motor = motor;
+		logger = new LogKitten(LogKitten.LEVEL_DEBUG, LogKitten.LEVEL_DEBUG);
 		this.encoder = encoder;
 		setOutputRange(-1, 1);
 		getPIDController().setContinuous(false);
@@ -24,6 +28,7 @@ public class EncodedMotor extends PIDSubsystem implements SpeedController {
 	}
 	
 	protected void usePIDOutput(double speed) {
+		logger.d(Double.toString(speed), true);
 		motor.set(speed);
 	}
 	
@@ -41,5 +46,28 @@ public class EncodedMotor extends PIDSubsystem implements SpeedController {
 	
 	public void set(double speed, byte arg1) {
 		set(speed);
+	}
+	private EncodedMotorMode pipeMode;
+	
+	public enum EncodedMotorMode {
+		PIDSET, SET;
+	}
+	
+	public void writePipe(double[] data) {
+		switch (pipeMode) {
+			case PIDSET:
+				set(data[0]);
+				return;
+			case SET:
+				motor.set(data[0]);
+				return;
+			default:
+				set(data[0]);
+				return;
+		}
+	}
+	
+	public void setPipe(Enum mode) {
+		pipeMode = (EncodedMotorMode) mode;
 	}
 }
