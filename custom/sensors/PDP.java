661b7b3bb3c7374662522fd3301812fbda4dbaa5
Carter Turnbaugh <carter@localhost.localdomain>
2015-08-30 12:30:06 -0700

    Trying to add LiveWindow to PDP

--- custom/sensors/PDP.java
+++ custom/sensors/PDP.java
@@ -2,9 +2,12 @@ package org.usfirst.frc4904.cmdbased.custom.sensors;
 
 
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 public class PDP extends PowerDistributionPanel {
 	public PDP() {}
 	
-	public void addLiveWindow(int port) {}
+	public void addLiveWindow() {
+		LiveWindow.addSensor("PDP", "PDP", this);
+	}
 }
