afb46f34231735571d1b393997644bfbeca6d487
Carter Turnbaugh <carter@localhost.localdomain>
2015-08-30 11:10:11 -0700

    Added CustomLimitSwitch, which acts as a trigger

---
+++ custom/sensors/CustomLimitSwitch.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc4904.cmdbased.custom.sensors;
+
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.buttons.Button;
+
+public class CustomLimitSwitch extends Button {
+	private final DigitalInput limitSwitch;
+	
+	public CustomLimitSwitch(DigitalInput limitSwitch) {
+		this.limitSwitch = limitSwitch;
+	}
+	
+	public CustomLimitSwitch(int port) {
+		this(new DigitalInput(port));
+	}
+	
+	public boolean get() {
+		return limitSwitch.get();
+	}
+}
