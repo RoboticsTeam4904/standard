b1598f03742e9d4a3c5740cb5318c012ad977ccc
Yoni Lerner <yonilern@gmail.com>
2015-09-25 16:11:38 -0700

    Added motor invertability

--- subsystems/motor/Motor.java
+++ subsystems/motor/Motor.java
@@ -7,11 +7,16 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Motor extends Subsystem implements SpeedController {
 	protected final SpeedController motor;
-	private final String haha;
+	protected boolean inverted;
+	
+	public Motor(String name, SpeedController motor, boolean inverted) {
+		super(name);
+		this.motor = motor;
+		this.inverted = inverted;
+	}
 	
 	public Motor(String name, SpeedController motor) {
-		this.motor = motor;
-		haha = name;
+		this(name, motor, false);
 	}
 	
 	protected void initDefaultCommand() {
@@ -19,7 +24,11 @@ public class Motor extends Subsystem implements SpeedController {
 	}
 	
 	public void pidWrite(double arg0) {
-		motor.pidWrite(arg0);
+		if (!inverted) {
+			motor.pidWrite(arg0);
+		} else {
+			motor.pidWrite(-1 * arg0);
+		}
 	}
 	
 	public void disable() {
@@ -27,17 +36,26 @@ public class Motor extends Subsystem implements SpeedController {
 	}
 	
 	public double get() {
-		return motor.get();
+		if (!inverted) {
+			return motor.get();
+		} else {
+			return -1 * motor.get();
+		}
 	}
 	
 	public void set(double arg0) {
-		motor.set(arg0);
-		if (haha == "WinchMotor") {
-			System.out.println(Thread.currentThread().getStackTrace()[2].getMethodName() + Thread.currentThread().getStackTrace()[3].getClassName() + " : " + haha + ": " + arg0);
+		if (!inverted) {
+			motor.set(arg0);
+		} else {
+			motor.set(-1 * arg0);
 		}
 	}
 	
 	public void set(double arg0, byte arg1) {
-		motor.set(arg0, arg1);
+		if (!inverted) {
+			motor.set(arg0, arg1);
+		} else {
+			motor.set(-1 * arg0, arg1);
+		}
 	}
 }
