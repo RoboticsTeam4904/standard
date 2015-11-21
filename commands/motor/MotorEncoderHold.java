04af59b13ebd5ea47ca4d9385e90fa6c9d68e8f1
Carter Turnbaugh <carter@localhost.localdomain>
2015-11-21 12:33:08 -0800

    Added the additional variables to the motor encoder commands.
    Now these commands will correctly handle their
    maximum and minimum inputs. Encoder
    distance per pulse will be handled by the
    EncodedMotor or the encoder.x

--- commands/motor/MotorEncoderHold.java
+++ commands/motor/MotorEncoderHold.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc4904.standard.commands.motor;
 
 
+import org.usfirst.frc4904.standard.subsystems.motor.EncodedMotor;
 import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDController;
@@ -13,12 +14,34 @@ public class MotorEncoderHold extends MotorSet implements PIDOutput {
 	protected Encoder encoder;
 	protected PIDController pid;
 	
+	/**
+	 * 
+	 * @param motor
+	 * @param encoder
+	 * @param P
+	 * @param I
+	 * @param D
+	 * @param minimum
+	 * @param maximum
+	 */
 	public MotorEncoderHold(Motor motor, Encoder encoder, double P, double I, double D, double minimum, double maximum) {
 		super(motor);
 		this.encoder = encoder;
 		pid = new PIDController(P, I, D, encoder, this);
 		pid.setInputRange(minimum, maximum);
 		pid.setOutputRange(-1.0, 1.0);
+		encoder.setPIDSourceParameter(Encoder.PIDSourceParameter.kDistance);
+	}
+	
+	public MotorEncoderHold(EncodedMotor motor) {
+		this(motor, motor.getEncoder(), motor.getP(), motor.getI(), motor.getD(), motor.getMinimum(), motor.getMaximum());
+	}
+	
+	public static MotorEncoderHold tryCastMotor(Motor motor) {
+		if (motor instanceof EncodedMotor) {
+			return new MotorEncoderHold((EncodedMotor) motor);
+		}
+		return null;
 	}
 	
 	public void setPosition(double position) {
