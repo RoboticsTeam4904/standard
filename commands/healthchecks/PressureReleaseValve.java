166c8375a922381498ee11be483b59163a713e0e
Carter Turnbaugh <carter@localhost.localdomain>
2016-01-17 10:31:37 -0800

    Pressure Release Valve.

--- commands/healthchecks/PressureReleaseValve.java
+++ commands/healthchecks/PressureReleaseValve.java
@@ -2,15 +2,20 @@ package org.usfirst.frc4904.standard.commands.healthchecks;
 
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class PressureReleaseValve extends AbstractHealthcheck {
 	private final Compressor compressor;
+	private final Solenoid solenoid;
 	private boolean valveClosed;
+	private byte solenoidsInitialValue;
 	
-	public PressureReleaseValve(String name, Compressor compressor) {
+	public PressureReleaseValve(String name, Compressor compressor, Solenoid solenoid) {
 		super(name, new InterruptCompressor(compressor));
 		this.compressor = compressor;
+		this.solenoid = solenoid;
 		valveClosed = false;
+		solenoidsInitialValue = solenoid.getAll();
 	}
 	
 	protected void initialize() {
@@ -26,7 +31,11 @@ public class PressureReleaseValve extends AbstractHealthcheck {
 		if (!getTimed()) {
 			return HealthStatus.SAFE; // If the compressor is running for less than 15 seconds, we are safe, not uncertain
 		}
-		return HealthStatus.DANGEROUS; // This means the compressor has been running for 15 seconds and we have had no increase in pressure
+		if (solenoid.getAll() != solenoidsInitialValue) {
+			return HealthStatus.DANGEROUS; // This means the compressor has been running for 15 seconds and we have had no increase in pressure (and no solenoids have been pressed)
+		}
+		solenoidsInitialValue = solenoid.getAll();
+		return HealthStatus.UNCERTAIN;
 	}
 	
 	protected boolean finished() {
