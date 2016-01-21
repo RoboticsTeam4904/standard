2af32f41a8373033a8592718e21c68c1f598bbdd
Carter Turnbaugh <carter@localhost.localdomain>
2016-01-20 22:12:37 -0800

    Javadoced controllers.

--- custom/controllers/CustomXbox.java
+++ custom/controllers/CustomXbox.java
@@ -1,11 +1,21 @@
 package org.usfirst.frc4904.standard.custom.controllers;
 
 
+/**
+ * An XboxController that implements
+ * the generic controller class.
+ *
+ */
 public class CustomXbox extends XboxController implements Controller {
 	public CustomXbox(final int port) {
 		super(port);
 	}
 	
+	/**
+	 * Axis 0: left joystick x
+	 * Axis 1: left joystick y
+	 * Axis 2: right joystick x
+	 */
 	public double getAxis(int axis) {
 		switch (axis) {
 			case 0:
--- custom/controllers/SuperButton.java
+++ custom/controllers/SuperButton.java
@@ -4,15 +4,23 @@ package org.usfirst.frc4904.standard.custom.controllers;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
+/**
+ * A button with better toggle detection
+ *
+ */
 public class SuperButton extends JoystickButton {
 	private boolean currentState;
 	
 	public SuperButton(GenericHID joystick, int buttonNumber) {
 		super(joystick, buttonNumber);
-		// TODO Auto-generated constructor stub
 		currentState = false;
 	}
 	
+	/**
+	 * Returns true the first time
+	 * the button is pressed and
+	 * the function is called.
+	 */
 	public boolean get() {
 		boolean buttonVal = super.get();
 		if (currentState != buttonVal) {
@@ -22,6 +30,10 @@ public class SuperButton extends JoystickButton {
 		return false;
 	}
 	
+	/**
+	 * Returns true whenever the button
+	 * is depressed.
+	 */
 	public boolean getRaw() {
 		return super.get();
 	}
