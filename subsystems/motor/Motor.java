9d958cc7deb971d5855517b93401d48e6646ff11
Yoni Lerner <yonilern@gmail.com>
2016-02-09 21:50:21 -0800

    Corrected LinearModifier name in Motor

--- subsystems/motor/Motor.java
+++ subsystems/motor/Motor.java
@@ -2,7 +2,7 @@ package org.usfirst.frc4904.standard.subsystems.motor;
 
 
 import org.usfirst.frc4904.standard.commands.motor.MotorIdle;
-import org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers.Linear;
+import org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers.LinearModifier;
 import org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers.SpeedModifier;
 import edu.wpi.first.wpilibj.CANSpeedController;
 import edu.wpi.first.wpilibj.SpeedController;
@@ -32,7 +32,7 @@ public class Motor extends Subsystem implements SpeedController {
 	}
 	
 	public Motor(String name, boolean inverted, SpeedController... motors) {
-		this(name, inverted, new Linear(), motors);
+		this(name, inverted, new LinearModifier(), motors);
 	}
 	
 	public Motor(String name, SpeedModifier slopeController, SpeedController... motors) {
@@ -40,7 +40,7 @@ public class Motor extends Subsystem implements SpeedController {
 	}
 	
 	public Motor(String name, SpeedController... motors) {
-		this(name, false, new Linear(), motors);
+		this(name, false, new LinearModifier(), motors);
 	}
 	
 	@Override
