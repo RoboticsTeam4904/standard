5c883594f75864a520966213c9acdeca411530d0
Yoni Lerner <yonilern@gmail.com>
2016-02-10 18:38:03 -0800

    Made CustomJoystick use SuperButton

--- custom/controllers/CustomJoystick.java
+++ custom/controllers/CustomJoystick.java
@@ -3,8 +3,6 @@ package org.usfirst.frc4904.standard.custom.controllers;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * A joystick that implements the generic
@@ -22,34 +20,34 @@ public class CustomJoystick extends Joystick implements Controller {
 	private static final double moveThreshold = 0.05;
 	private final int port;
 	// Buttons
-	public final Button button1;
-	public final Button button2;
-	public final Button button3;
-	public final Button button4;
-	public final Button button5;
-	public final Button button6;
-	public final Button button7;
-	public final Button button8;
-	public final Button button9;
-	public final Button button10;
-	public final Button button11;
-	public final Button button12;
+	public final SuperButton button1;
+	public final SuperButton button2;
+	public final SuperButton button3;
+	public final SuperButton button4;
+	public final SuperButton button5;
+	public final SuperButton button6;
+	public final SuperButton button7;
+	public final SuperButton button8;
+	public final SuperButton button9;
+	public final SuperButton button10;
+	public final SuperButton button11;
+	public final SuperButton button12;
 	
 	public CustomJoystick(int port) {
 		super(port);
 		this.port = port;
-		button1 = new JoystickButton(this, 1);
-		button2 = new JoystickButton(this, 2);
-		button3 = new JoystickButton(this, 3);
-		button4 = new JoystickButton(this, 4);
-		button5 = new JoystickButton(this, 5);
-		button6 = new JoystickButton(this, 6);
-		button7 = new JoystickButton(this, 7);
-		button8 = new JoystickButton(this, 8);
-		button9 = new JoystickButton(this, 9);
-		button10 = new JoystickButton(this, 10);
-		button11 = new JoystickButton(this, 11);
-		button12 = new JoystickButton(this, 12);
+		button1 = new SuperButton(this, 1);
+		button2 = new SuperButton(this, 2);
+		button3 = new SuperButton(this, 3);
+		button4 = new SuperButton(this, 4);
+		button5 = new SuperButton(this, 5);
+		button6 = new SuperButton(this, 6);
+		button7 = new SuperButton(this, 7);
+		button8 = new SuperButton(this, 8);
+		button9 = new SuperButton(this, 9);
+		button10 = new SuperButton(this, 10);
+		button11 = new SuperButton(this, 11);
+		button12 = new SuperButton(this, 12);
 	}
 	
 	/**
