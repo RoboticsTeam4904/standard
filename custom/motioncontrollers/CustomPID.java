5faa7bfbada1b9702cf39a89a5e0cd73680c2c9b
Yoni Lerner <yonilern@gmail.com>
2016-02-16 17:42:36 -0800

    Improved and documented CustomPID.get()

--- custom/motioncontrollers/CustomPID.java
+++ custom/motioncontrollers/CustomPID.java
@@ -58,40 +58,55 @@ public class CustomPID extends MotionController {
 		this.F = F;
 	}
 	
+	@Override
 	public void reset() {
 		setpoint = source.pidGet();
 		totalError = 0;
 		lastError = 0;
 	}
 	
+	@Override
 	public double getError() {
 		return lastError;
 	}
 	
+	@Override
+	/**
+	 * Get the current output of the PID loop.
+	 * This should be used to set the output (like a Motor).
+	 * 
+	 * @return The current output of the PID loop.
+	 */
 	public double get() {
+		// If PID is not enabled, use feedforward only
 		if (!enable) {
 			return F * setpoint;
 		}
 		double input = source.pidGet();
 		double deltaT = (System.currentTimeMillis() - lastUpdate) / 1000.0;
 		double error = setpoint - input;
+		// Account for continuous input ranges
 		if (continuous) {
-			if (Math.abs(error) > (inputMax - inputMin) / 2) {
+			double range = inputMax - inputMin;
+			// If the error is more than half of the range, it is faster to increase the error and loop around the boundary
+			if (Math.abs(error) > range / 2) {
 				if (error > 0) {
-					error = error - inputMax + inputMin;
+					error -= range;
 				} else {
-					error = error + inputMax - inputMin;
+					error += range;
 				}
 			}
 		}
+		// Calculate the approximation of the error's derivative
+		double errorDerivative = (error - lastError) / deltaT;
+		// Calculate the approximation of the error's integral
 		totalError += error * deltaT;
-		double result = P * error + I * totalError + D * ((error - lastError) / deltaT) + F * setpoint;
+		// Calculate the result using the PIDF formula
+		double result = P * error + I * totalError + D * errorDerivative + F * setpoint;
+		// Limit the result to be within the output range [outputMin, outputMax]
+		result = Math.max(Math.min(result, outputMax), outputMin);
+		// Save the error for calculating future derivatives
 		lastError = error;
-		if (result > outputMax) {
-			return outputMax;
-		} else if (result < outputMin) {
-			return outputMin;
-		}
 		return result;
 	}
 }
