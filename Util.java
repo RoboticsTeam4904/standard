d975075063d065c1f97d78fe3416785b6b0a6d06
Carter Turnbaugh <carturn@nuevaschool.org>
2016-02-18 09:57:53 -0800

    Improved Javadocing.

--- Util.java
+++ Util.java
@@ -6,15 +6,13 @@ package org.usfirst.frc4904.standard;
  */
 public class Util {
 	/**
-	 * A function for determining if floating point
-	 * numbers are effectively zero.
-	 * Floating point arithmetic tends to introduce
-	 * very small errors.
+	 * A function for determining if floating point numbers are effectively zero.
+	 * Floating point arithmetic tends to introduce very small errors.
 	 * 
 	 * @param var
 	 *        The floating point number you want to compare
 	 * @return
-	 * 		Whether or not it is within Constants.epsilon of zero
+	 * 		Whether or not it is within Constants.EPSILON of zero
 	 */
 	public static boolean isZero(double var) {
 		return Math.abs(var) < Constants.EPSILON;
