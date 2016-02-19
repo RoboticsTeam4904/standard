48f01251fd8c91713407f739daceee52f7ea4a6e
Yoni Lerner <yonilern@gmail.com>
2016-02-18 19:15:12 -0800

    Added right stick Y

--- custom/controllers/CustomXbox.java
+++ custom/controllers/CustomXbox.java
@@ -15,15 +15,18 @@ public class CustomXbox extends XboxController implements Controller {
 	 * Axis 0: left joystick x
 	 * Axis 1: left joystick y
 	 * Axis 2: right joystick x
+	 * Axis 2: right joystick y
 	 */
 	public double getAxis(int axis) {
 		switch (axis) {
 			case 0:
-				return this.leftStick.getX();
+				return leftStick.getX();
 			case 1:
-				return this.leftStick.getY();
+				return leftStick.getY();
 			case 2:
-				return this.rightStick.getX();
+				return rightStick.getX();
+			case 3:
+				return rightStick.getY();
 			default:
 				return super.getRawAxis(axis);
 		}
