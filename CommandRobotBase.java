e30d8ae848699d04e34e561c2ca3f6bfc553ffaf
Carter Turnbaugh <carturn@nuevaschool.org>
2016-02-29 12:16:45 -0800

    Added logging of Driver and Operator choices.

--- CommandRobotBase.java
+++ CommandRobotBase.java
@@ -74,9 +74,11 @@ public abstract class CommandRobotBase extends IterativeRobot {
 			autonomousCommand.cancel();
 		}
 		if (driverChooser.getSelected() != null) {
+			LogKitten.d("Loading driver " + driverChooser.getSelected().getName());
 			driverChooser.getSelected().bindCommands();
 		}
 		if (operatorChooser.getSelected() != null) {
+			LogKitten.d("Loading operator " + operatorChooser.getSelected().getName());
 			operatorChooser.getSelected().bindCommands();
 		}
 		teleopInitialize();
