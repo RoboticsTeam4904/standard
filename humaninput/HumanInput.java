0f939867880c2c9c9942dfa64175e64a692d0827
Carter Turnbaugh <carturn@nuevaschool.org>
2016-03-20 12:17:23 -0700

    Added driver specific overrides

--- humaninput/HumanInput.java
+++ humaninput/HumanInput.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc4904.standard.humaninput;
 
 
-import org.usfirst.frc4904.standard.custom.Named;
+import java.util.ArrayList;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  * A generic human interface class.
@@ -11,8 +13,9 @@ import org.usfirst.frc4904.standard.custom.Named;
  * during teleop init.
  *
  */
-public abstract class HumanInput implements Named {
+public abstract class HumanInput extends Command {
 	protected final String name;
+	private final ArrayList<SubsystemDefaultOverrider> subsystemDefaultOverriders = new ArrayList<SubsystemDefaultOverrider>();
 	
 	public HumanInput(String name) {
 		this.name = name;
@@ -23,9 +26,77 @@ public abstract class HumanInput implements Named {
 		return name;
 	}
 	
+	@Override
+	protected void initialize() {
+		bindCommands();
+	}
+	
+	@Override
+	protected void execute() {
+		for (SubsystemDefaultOverrider subsystemDefaultOverrider : subsystemDefaultOverriders) {
+			subsystemDefaultOverrider.execute();
+		}
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+	
+	@Override
+	protected void end() {}
+	
+	@Override
+	protected void interrupted() {}
+	
 	/**
 	 * A function where the driver's and operator's controls are bound to commands
 	 * Can't be done in the constructor because constructors are called too early
 	 */
 	public abstract void bindCommands();
+	
+	/**
+	 * 
+	 * @param subsystem
+	 * @param newDefault
+	 * @param oldDefault
+	 */
+	protected void overrideDefault(Subsystem subsystem, Command newDefault, Command oldDefault) {
+		for (SubsystemDefaultOverrider subsystemDefaultOverrider : subsystemDefaultOverriders) {
+			if (subsystemDefaultOverrider.getSubsystem() == subsystem) {
+				subsystemDefaultOverrider.changeNewDefault(newDefault);
+				return;
+			}
+		}
+		subsystemDefaultOverriders.add(new SubsystemDefaultOverrider(subsystem, newDefault, oldDefault));
+	}
+	
+	private class SubsystemDefaultOverrider {
+		private final Subsystem subsystem;
+		private Command newDefault;
+		private final Command oldDefault;
+		
+		public SubsystemDefaultOverrider(Subsystem subsystem, Command newDefault, Command oldDefault) {
+			this.subsystem = subsystem;
+			this.newDefault = newDefault;
+			this.oldDefault = oldDefault;
+		}
+		
+		public void execute() {
+			if (subsystem.getCurrentCommand().getClass() == oldDefault.getClass()) {
+				newDefault.start();
+			}
+		}
+		
+		public void changeNewDefault(Command newDefault) {
+			if (newDefault.isRunning()) {
+				newDefault.cancel();
+			}
+			this.newDefault = newDefault;
+		}
+		
+		public Subsystem getSubsystem() {
+			return subsystem;
+		}
+	}
 }
