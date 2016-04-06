b693a69ccbe96b7581717ecb1d3aa5856f2aa0a6
Yoni Lerner <yonilern@gmail.com>
2016-04-06 10:21:56 -0700

    Fixed calculation to account for starting encoder values

--- commands/chassis/ChassisSetDistance.java
+++ commands/chassis/ChassisSetDistance.java
@@ -8,19 +8,24 @@ import org.usfirst.frc4904.standard.subsystems.chassis.Chassis;
 public class ChassisSetDistance extends ChassisConstant {
 	protected CustomEncoder[] motorEncoders;
 	protected double distance;
-
+	protected double[] initialDistances;
+	
 	public ChassisSetDistance(Chassis chassis, double distance, double speed, CustomEncoder... motorEncoders) {
 		super(chassis, 0.0, speed, 0.0, Double.MAX_VALUE);
 		this.motorEncoders = motorEncoders;
 		this.distance = distance;
+		initialDistances = new double[motorEncoders.length];
+		for (int i = 0; i < motorEncoders.length; i++) {
+			initialDistances[i] = motorEncoders[i].getDistance();
+		}
 	}
 
 	@Override
 	protected boolean isFinished() {
 		double distanceSum = 0;
 		for (int i = 0; i < motorEncoders.length; i++) {
-			distanceSum += motorEncoders[i].getDistance();
 			LogKitten.v("Encoder " + i + " reads " + motorEncoders[i].getDistance() + "out of " + distance);
+			distanceSum += motorEncoders[i].getDistance() - initialDistances[i];
 		}
 		double distanceAvg = distanceSum / motorEncoders.length;
 		LogKitten.v((distanceAvg > distance) + "");
