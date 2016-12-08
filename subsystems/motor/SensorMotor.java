3f7e63e4464b5f28d6e35d68500b261ab4f7f0fc
Carter Turnbaugh <carturn@nuevaschool.org>
2016-12-07 23:08:25 -0800

    Removed functions from sensorMotor

--- subsystems/motor/SensorMotor.java
+++ subsystems/motor/SensorMotor.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc4904.standard.subsystems.motor;
 
 
-import org.usfirst.frc4904.standard.LogKitten;
 import org.usfirst.frc4904.standard.custom.motioncontrollers.MotionController;
 import org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers.IdentityModifier;
 import org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers.SpeedModifier;
@@ -9,70 +8,56 @@ import edu.wpi.first.wpilibj.SpeedController;
 
 public abstract class SensorMotor extends Motor {
 	protected final MotionController motionController;
-	
+
 	public SensorMotor(String name, boolean inverted, SpeedModifier speedModifier, MotionController motionController, SpeedController... motors) {
 		super(name, inverted, speedModifier, motors);
 		this.motionController = motionController;
 	}
-	
+
 	public SensorMotor(String name, boolean isInverted, MotionController motionController, SpeedController... motors) {
 		this(name, isInverted, new IdentityModifier(), motionController, motors);
 	}
-	
+
 	public SensorMotor(String name, SpeedModifier speedModifier, MotionController motionController, SpeedController... motors) {
 		this(name, false, speedModifier, motionController, motors);
 	}
-	
+
 	public SensorMotor(String name, MotionController motionController, SpeedController... motors) {
 		this(name, false, new IdentityModifier(), motionController, motors);
 	}
-	
+
 	public SensorMotor(boolean isInverted, SpeedModifier speedModifier, MotionController motionController, SpeedController... motors) {
 		this("SensorMotor", isInverted, speedModifier, motionController, motors);
 	}
-	
+
 	public SensorMotor(boolean isInverted, MotionController motionController, SpeedController... motors) {
 		this("SensorMotor", isInverted, motionController, motors);
 	}
-	
+
 	public SensorMotor(SpeedModifier speedModifier, MotionController motionController, SpeedController... motors) {
 		this("SensorMotor", speedModifier, motionController, motors);
 	}
-	
+
 	public SensorMotor(MotionController motionController, SpeedController... motors) {
 		this("SensorMotor", motionController, motors);
 	}
-	
+
 	public void reset() {
 		motionController.reset();
 	}
-	
+
 	public void setInputRange(double minimum, double maximum) {}
-	
+
 	public void enableMC() {
 		motionController.setOutput(this);
 		motionController.enable();
 	}
-	
+
 	public void disableMC() {
 		motionController.disable();
 	}
-	
-	public void setPosition(double position) {
-		motionController.setSetpoint(position);
-	}
-	
+
 	public boolean onTarget() {
 		return motionController.onTarget();
-	}
-	
-	@Override
-	public void set(double speed) {
-		LogKitten.v(speed + "");
-		if (motionController.isEnabled()) {
-			motionController.setSetpoint(speed);
-		} else {
-			super.set(speed);
-		}
 	}
 }
