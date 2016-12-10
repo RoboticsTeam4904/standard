b6fc6a9339ab5254a1b5a9633aabe47358396f8e
ave4224 <avery@bvp.com>
2016-12-09 17:59:51 -0800

    updated

--- commands/healthchecks/HealthProtectCommand.java
+++ commands/healthchecks/HealthProtectCommand.java
@@ -4,7 +4,8 @@ package org.usfirst.frc4904.standard.commands.healthchecks;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- * Abstract Command to allow for a reset function
+ * Abstract Command to allow for a reset function. This should
+ * be used when a Health Check might need to reset a Command.
  */
 public abstract class HealthProtectCommand extends Command {
 	/**
