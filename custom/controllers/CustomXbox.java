cc5297257086bee0997bb17b04d049c9ca6eee58
blobfish2000@gmail.com <blobfish2000@gmail.com>
2017-01-30 14:15:39 -0800

    initial custom buttons

--- custom/controllers/CustomXbox.java
+++ custom/controllers/CustomXbox.java
@@ -2,8 +2,7 @@ package org.usfirst.frc4904.standard.custom.controllers;
 
 
 /**
- * An XboxController that implements
- * the generic controller class.
+ * An XboxController that implements the generic controller class.
  *
  */
 public class CustomXbox extends XboxController implements Controller {
@@ -11,30 +10,44 @@ public class CustomXbox extends XboxController implements Controller {
 	public final static int LEFT_Y_AXIS = 1;
 	public final static int RIGHT_X_AXIS = 2;
 	public final static int RIGHT_Y_AXIS = 3;
-	
+	public CustomButton a;
+	public CustomButton b;
+	public CustomButton x;
+	public CustomButton y;
+	public CustomButton lb;
+	public CustomButton rb;
+	public CustomButton back;
+	public CustomButton start;
+
 	public CustomXbox(final int port) {
 		super(port);
+		a = new CustomButton(controller, XboxController.A_BUTTON_ID);
+		b = new CustomButton(controller, XboxController.B_BUTTON_ID);
+		x = new CustomButton(controller, XboxController.X_BUTTON_ID);
+		y = new CustomButton(controller, XboxController.Y_BUTTON_ID);
+		lb = new CustomButton(controller, XboxController.LB_BUTTON_ID);
+		rb = new CustomButton(controller, XboxController.RB_BUTTON_ID);
+		back = new CustomButton(controller, XboxController.BACK_BUTTON_ID);
+		start = new CustomButton(controller, XboxController.START_BUTTON_ID);
 	}
-	
+
 	/**
-	 * Axis 0: left joystick x
-	 * Axis 1: left joystick y
-	 * Axis 2: right joystick x
+	 * Axis 0: left joystick x Axis 1: left joystick y Axis 2: right joystick x
 	 * Axis 2: right joystick y
 	 */
 	@Override
 	public double getAxis(int axis) {
 		switch (axis) {
-			case 0:
-				return leftStick.getX();
-			case 1:
-				return leftStick.getY();
-			case 2:
-				return rightStick.getX();
-			case 3:
-				return rightStick.getY();
-			default:
-				return super.getRawAxis(axis);
+		case 0:
+			return leftStick.getX();
+		case 1:
+			return leftStick.getY();
+		case 2:
+			return rightStick.getX();
+		case 3:
+			return rightStick.getY();
+		default:
+			return super.getRawAxis(axis);
 		}
 	}
 }
