488393540d52a3e4175e4c4db0392074a5c7ff86
AJ Nadel <aj@ajnadel.com>
2017-02-02 18:38:46 -0800

    finish xbox class update

--- custom/controllers/CustomXbox.java
+++ custom/controllers/CustomXbox.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc4904.standard.custom.controllers;
 
+
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.Button;
 
@@ -8,12 +9,16 @@ import edu.wpi.first.wpilibj.buttons.Button;
  *
  */
 public class CustomXbox extends XboxController implements Controller {
-	public final static int LEFT_X_AXIS = 0;
-	public final static int LEFT_Y_AXIS = 1;
-	public final static int RIGHT_X_AXIS = 2;
-	public final static int RIGHT_Y_AXIS = 3;
-	public final static int LEFT_TRIGGER_AXIS = 4;
-	public final static int RIGHT_TRIGGER_AXIS = 5;
+	// public enum Axis {
+	// LEFT_X, LEFT_Y, RIGHT_X, RIGHT_Y, LEFT_TRIGGER, RIGHT_TRIGGER;
+	// private double deadzone = CustomXbox.DEFAULT_DEADZONE;
+	// }
+	private final static int LEFT_X_AXIS = 0;
+	private final static int LEFT_Y_AXIS = 1;
+	private final static int RIGHT_X_AXIS = 2;
+	private final static int RIGHT_Y_AXIS = 3;
+	private final static int LEFT_TRIGGER_AXIS = 4;
+	private final static int RIGHT_TRIGGER_AXIS = 5;
 	protected static final int A_BUTTON_ID = 1;
 	protected static final int B_BUTTON_ID = 2;
 	protected static final int X_BUTTON_ID = 3;
@@ -24,26 +29,21 @@ public class CustomXbox extends XboxController implements Controller {
 	protected static final int START_BUTTON_ID = 8;
 	protected static final int LEFT_THUMBSTICK_BUTTON_ID = 9;
 	protected static final int RIGHT_THUMBSTICK_BUTTON_ID = 10;
-	private double THUMBSTICK_DEADZONE = 0.1;
-	private double TRIGGER_DEADZONE = 0.01;
 	public CustomButton a;
 	public CustomButton b;
 	public CustomButton x;
 	public CustomButton y;
-	public CustomButton lb;
-	public CustomButton rb;
+	public CustomButton leftBumper;
+	public CustomButton rightBumper;
 	public CustomButton back;
 	public CustomButton start;
-	public CustomButton lstickbutton;
-	public CustomButton rstickbutton;
-	public final Button up;
-	public final Button upRight;
-	public final Button right;
-	public final Button downRight;
-	public final Button down;
-	public final Button downLeft;
-	public final Button left;
-	public final Button upLeft;
+	public CustomButton leftStickButton;
+	public CustomButton rightStickButton;
+	public Stick leftStick;
+	public Stick rightStick;
+	public AnalogInput leftTrigger;
+	public AnalogInput rightTrigger;
+	public DPad dPad;
 
 	public CustomXbox(final int port) {
 		super(port);
@@ -51,27 +51,82 @@ public class CustomXbox extends XboxController implements Controller {
 		b = new CustomButton(this, CustomXbox.B_BUTTON_ID);
 		x = new CustomButton(this, CustomXbox.X_BUTTON_ID);
 		y = new CustomButton(this, CustomXbox.Y_BUTTON_ID);
-		lb = new CustomButton(this, CustomXbox.LB_BUTTON_ID);
-		rb = new CustomButton(this, CustomXbox.RB_BUTTON_ID);
+		leftBumper = new CustomButton(this, CustomXbox.LB_BUTTON_ID);
+		rightBumper = new CustomButton(this, CustomXbox.RB_BUTTON_ID);
 		back = new CustomButton(this, CustomXbox.BACK_BUTTON_ID);
 		start = new CustomButton(this, CustomXbox.START_BUTTON_ID);
-		lstickbutton = new CustomButton(this, CustomXbox.LEFT_THUMBSTICK_BUTTON_ID);
-		rstickbutton = new CustomButton(this, CustomXbox.RIGHT_THUMBSTICK_BUTTON_ID);
-		up = new DPadButton(this, 0);
-		upRight = new DPadButton(this, 45);
-		right = new DPadButton(this, 90);
-		downRight = new DPadButton(this, 135);
-		down = new DPadButton(this, 180);
-		downLeft = new DPadButton(this, 225);
-		left = new DPadButton(this, 270);
-		upLeft = new DPadButton(this, 315);
+		leftStickButton = new CustomButton(this, CustomXbox.LEFT_THUMBSTICK_BUTTON_ID);
+		rightStickButton = new CustomButton(this, CustomXbox.RIGHT_THUMBSTICK_BUTTON_ID);
+		leftStick = new Stick(this, CustomXbox.LEFT_X_AXIS, CustomXbox.LEFT_Y_AXIS);
+		rightStick = new Stick(this, CustomXbox.RIGHT_X_AXIS, CustomXbox.RIGHT_Y_AXIS);
+		leftTrigger = new AnalogInput(this, CustomXbox.LEFT_TRIGGER_AXIS);
+		rightTrigger = new AnalogInput(this, CustomXbox.RIGHT_TRIGGER_AXIS);
+		dPad = new DPad(this);
+	}
+
+	public static class AnalogInput {
+		public static final double DEFAULT_DEADZONE = 0.05;
+		protected double deadzone = AnalogInput.DEFAULT_DEADZONE;
+		protected CustomXbox controller;
+		protected final int xIndex;
+
+		private AnalogInput(CustomXbox controller, int index) {
+			this.controller = controller;
+			xIndex = index;
+		}
+
+		public double getX() {
+			return controller.getAxis(xIndex);
+		}
+
+		public double getDeadzone() {
+			return deadzone;
+		}
+
+		public void setDeadzone(double deadzone) {
+			this.deadzone = deadzone;
+		}
+	}
+
+	public static class Stick extends AnalogInput {
+		protected final int yIndex;
+
+		private Stick(CustomXbox controller, int xIndex, int yIndex) {
+			super(controller, xIndex);
+			this.yIndex = yIndex;
+		}
+
+		public double getY() {
+			return controller.getAxis(yIndex);
+		}
+	}
+
+	public static class DPad {
+		public final Button up;
+		public final Button upRight;
+		public final Button right;
+		public final Button downRight;
+		public final Button down;
+		public final Button downLeft;
+		public final Button left;
+		public final Button upLeft;
+
+		public DPad(CustomXbox controller) {
+			up = new DPadButton(controller, 0);
+			upRight = new DPadButton(controller, 45);
+			right = new DPadButton(controller, 90);
+			downRight = new DPadButton(controller, 135);
+			down = new DPadButton(controller, 180);
+			downLeft = new DPadButton(controller, 225);
+			left = new DPadButton(controller, 270);
+			upLeft = new DPadButton(controller, 315);
+		}
 	}
 
 	/**
 	 * Axis 0: left joystick x Axis 1: left joystick y Axis 2: right joystick x
 	 * Axis 2: right joystick y
 	 */
-
 	public static double computeDeadzone(double input, double deadzone) {
 		if (deadzone < 0 || deadzone >= 1) {
 			deadzone = 0; // Prevent any weird errors
@@ -81,48 +136,34 @@ public class CustomXbox extends XboxController implements Controller {
 	}
 
 	/**
-	 * Get the adjusted thumbstick deadzone
-	 * 
-	 * @return deadzone
-	 */
-	public double getThumbstickDeadzone() {
-		return THUMBSTICK_DEADZONE;
-	}
-
-	/**
-	 * Get the adjusted trigger deadzone
-	 * 
-	 * @return deadzone
-	 */
-	public double getTriggerDeadzone() {
-		return TRIGGER_DEADZONE;
-	}
-
-	/**
-	 * Set the deadzone of the thumbsticks
+	 * Set the deadzone of the sticks
 	 * 
 	 * @param deadzone
+	 *        the deadzone
 	 */
-	public void setThumbstickDeadzone(double deadzone) {
-		THUMBSTICK_DEADZONE = deadzone;
+	public void setStickDeadzone(double deadzone) {
+		leftStick.setDeadzone(deadzone);
+		rightStick.setDeadzone(deadzone);
 	}
 
 	/**
-	 * Set the deadzone of the thumbsticks
+	 * Set the deadzone of the triggers
 	 * 
 	 * @param deadzone
+	 *        the deadzone
 	 */
 	public void setTriggerDeadzone(double deadzone) {
-		TRIGGER_DEADZONE = deadzone;
+		leftTrigger.setDeadzone(deadzone);
+		rightTrigger.setDeadzone(deadzone);
 	}
 
 	/**
 	 * Make one side of the controller vibrate
 	 * 
 	 * @param hand
-	 *            The side of the controller to rumble
+	 *        The side of the controller to rumble
 	 * @param intensity
-	 *            How strong the rumble is
+	 *        How strong the rumble is
 	 */
 	public void setRumble(Hand hand, double intensity) {
 		final float amount = new Float(intensity);
@@ -137,7 +178,7 @@ public class CustomXbox extends XboxController implements Controller {
 	 * Make the controller vibrate
 	 * 
 	 * @param intensity
-	 *            How strong the rumble is
+	 *        How strong the rumble is
 	 */
 	public void setRumble(double intensity) {
 		final float amount = new Float(intensity);
@@ -147,22 +188,7 @@ public class CustomXbox extends XboxController implements Controller {
 
 	@Override
 	public double getAxis(int axis) {
-		switch (axis) {
-		case LEFT_X_AXIS:
-			return CustomXbox.computeDeadzone(this.getX(Hand.kLeft), THUMBSTICK_DEADZONE);
-		case LEFT_Y_AXIS:
-			return CustomXbox.computeDeadzone(this.getY(Hand.kLeft), THUMBSTICK_DEADZONE);
-		case RIGHT_X_AXIS:
-			return CustomXbox.computeDeadzone(this.getX(Hand.kRight), THUMBSTICK_DEADZONE);
-		case RIGHT_Y_AXIS:
-			return CustomXbox.computeDeadzone(this.getY(Hand.kRight), THUMBSTICK_DEADZONE);
-		case LEFT_TRIGGER_AXIS:
-			return CustomXbox.computeDeadzone(getTriggerAxis(Hand.kLeft), TRIGGER_DEADZONE);
-		case RIGHT_TRIGGER_AXIS:
-			return CustomXbox.computeDeadzone(getTriggerAxis(Hand.kRight), TRIGGER_DEADZONE);
-		default:
-			return super.getRawAxis(axis);
-		}
+		return super.getRawAxis(axis);
 	}
 
 	public static class DPadButton extends Button {
