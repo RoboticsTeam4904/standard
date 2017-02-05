e1f0daddedb82e272ca73c9c8bacfcf685acbcdd
Yoni Lerner <yonilern@gmail.com>
2017-02-05 11:27:44 -0800

    Changed to self constructor

--- subsystems/motor/speedmodifiers/CapSpeedModifier.java
+++ subsystems/motor/speedmodifiers/CapSpeedModifier.java
@@ -4,16 +4,16 @@ package org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers;
 import org.usfirst.frc4904.standard.Util;
 
 public class CapSpeedModifier implements SpeedModifier {
-	public CapSpeedModifier(double minSpeed, double maxSpeed) {
-		speedCap = new Util.Range(minSpeed, maxSpeed);
-	}
-	
 	protected final Util.Range speedCap;
 
 	public CapSpeedModifier(Util.Range speedCap) {
 		this.speedCap = speedCap;
 	}
-	
+
+	public CapSpeedModifier(double minSpeed, double maxSpeed) {
+		this(new Util.Range(minSpeed, maxSpeed));
+	}
+
 	@Override
 	public double modify(double speed) {
 		return speedCap.limitValue(speed);
