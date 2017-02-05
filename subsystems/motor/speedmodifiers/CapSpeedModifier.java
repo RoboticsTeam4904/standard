f4f586a7d1f39aa3296f1bb794385a00edee1c23
Yoni Lerner <yonilern@gmail.com>
2017-02-05 11:27:25 -0800

    Made speedCap final and protected

--- subsystems/motor/speedmodifiers/CapSpeedModifier.java
+++ subsystems/motor/speedmodifiers/CapSpeedModifier.java
@@ -4,12 +4,12 @@ package org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers;
 import org.usfirst.frc4904.standard.Util;
 
 public class CapSpeedModifier implements SpeedModifier {
-	public Util.Range speedCap;
-	
 	public CapSpeedModifier(double minSpeed, double maxSpeed) {
 		speedCap = new Util.Range(minSpeed, maxSpeed);
 	}
 	
+	protected final Util.Range speedCap;
+
 	public CapSpeedModifier(Util.Range speedCap) {
 		this.speedCap = speedCap;
 	}
