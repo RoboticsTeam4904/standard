87ca1d3d2af102f359b667e8fa0f0fe9538c930f
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2017-02-05 11:13:12 -0800

    added an option for user to give an Util.range instead of max and min values

--- subsystems/motor/speedmodifiers/CapSpeedModifier.java
+++ subsystems/motor/speedmodifiers/CapSpeedModifier.java
@@ -4,10 +4,14 @@ package org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers;
 import org.usfirst.frc4904.standard.Util;
 
 public class CapSpeedModifier implements SpeedModifier {
-	public final Util.Range speedCap;
+	public Util.Range speedCap;
 	
 	public CapSpeedModifier(double minSpeed, double maxSpeed) {
 		speedCap = new Util.Range(minSpeed, maxSpeed);
+	}
+	
+	public CapSpeedModifier(Util.Range speedCap) {
+		this.speedCap = speedCap;
 	}
 	
 	@Override
