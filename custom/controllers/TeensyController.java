37081c70c5b63dc00cc0396186176e5e65b67f58
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2017-02-18 15:26:51 -0800

    made some documentation

--- custom/controllers/TeensyController.java
+++ custom/controllers/TeensyController.java
@@ -7,15 +7,15 @@ import edu.wpi.first.wpilibj.Joystick;
 public class TeensyController extends Joystick implements Controller {
 	public int numButtons;
 	public final int port;
-	public CustomButton[] buttons;
+	public CustomButton[] buttons; // array of all buttons
 
 	public TeensyController(int port, int numButtons) {
 		super(port);
 		this.port = port;
 		this.numButtons = numButtons;
-		CustomButton[] buttons = new CustomButton[numButtons];
+		CustomButton[] buttons = new CustomButton[numButtons]; // array of any length
 		this.buttons = buttons;
-		for (int index = 0; index < numButtons; index++) {
+		for (int index = 0; index < numButtons; index++) { // sets each button in buttons
 			buttons[index] = new CustomButton(this, index);
 		}
 	}
@@ -32,15 +32,15 @@ public class TeensyController extends Joystick implements Controller {
 	}
 
 	@Override
-	public double getAxis(int axis) {
+	public double getAxis(int axis) { // gets the value of the axis
 		return super.getRawAxis(axis);
 	}
 
-	public double getNumButtons() {
+	public double getNumButtons() { // returns number of buttons
 		return numButtons;
 	}
 
-	public CustomButton getButton(int id) {
+	public CustomButton getButton(int id) { // allows for indirect access to array
 		return buttons[id];
 	}
 }
