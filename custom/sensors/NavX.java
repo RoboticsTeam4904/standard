4918442686594648b138c2f3e460bfc14f35fcb5
AJ Nadel <aj@ajnadel.com>
2017-02-19 15:19:58 -0800

    make all NavX getters use abs differences

--- custom/sensors/NavX.java
+++ custom/sensors/NavX.java
@@ -15,7 +15,7 @@ public class NavX extends AHRS implements IMU {
 	protected float lastRoll;
 	protected double lastYawRate;
 	protected static final double MAX_DEGREES_PER_TICK = 90.0;
-	protected static final double MAX_RATE_INCREASE_FACTOR_PER_TICK = 5.0;
+	protected static final double MAX_DEGREES_PER_SECOND_PER_TICK = 180;
 
 	public NavX(SerialPort.Port port) {
 		super(port);
@@ -37,7 +37,7 @@ public class NavX extends AHRS implements IMU {
 	@Override
 	public double getRate() {
 		double rate = super.getRate();
-		if (Math.abs(rate / lastYawRate) > NavX.MAX_RATE_INCREASE_FACTOR_PER_TICK) {
+		if (Math.abs(rate - lastYawRate) > NavX.MAX_DEGREES_PER_SECOND_PER_TICK) {
 			return lastYawRate;
 		}
 		lastYawRate = rate;
@@ -67,7 +67,7 @@ public class NavX extends AHRS implements IMU {
 	@Override
 	public float getPitch() {
 		float pitch = super.getPitch();
-		if (Math.abs(pitch) > Math.abs(lastPitch) + NavX.MAX_DEGREES_PER_TICK) {
+		if (Math.abs(pitch - lastPitch) > NavX.MAX_DEGREES_PER_TICK) {
 			return lastPitch;
 		}
 		if (pitch < 0) {
@@ -85,7 +85,7 @@ public class NavX extends AHRS implements IMU {
 	@Override
 	public float getRoll() {
 		float roll = super.getRoll();
-		if (Math.abs(roll) > Math.abs(lastRoll) + NavX.MAX_DEGREES_PER_TICK) {
+		if (Math.abs(roll lastRoll) > NavX.MAX_DEGREES_PER_TICK) {
 			return lastRoll;
 		}
 		if (roll < 0) {
