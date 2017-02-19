52c1f4ea9d2fdf5ae63d5de32c6fd8e51414ecb5
Carter Turnbaugh <carturn@nuevaschool.org>
2017-02-18 18:53:52 -0800

    Fixed failure cases for anti-brownout

--- subsystems/motor/speedmodifiers/AccelerationCap.java
+++ subsystems/motor/speedmodifiers/AccelerationCap.java
@@ -14,7 +14,7 @@ import edu.wpi.first.wpilibj.SpeedController;
 public class AccelerationCap implements SpeedModifier {
 	public final static double MINIMUM_OPERATING_VOLTAGE = 7.0;
 	public final static double MAXIMUM_MOTOR_INCREASE_PER_SECOND = 1.6;
-	public final static double MAXIMUM_MOTOR_DECREASE_PER_SECOND = 6.4;
+	public final static double MAXIMUM_MOTOR_DECREASE_PER_SECOND = 3.2;
 	public final static double ANTI_BROWNOUT_BACKOFF = 0.25; // How much to throttle a motor down to avoid brownout
 	public final static double DEFAULT_HARD_STOP_VOLTAGE = 9.0;
 	// Motor constants
@@ -94,14 +94,14 @@ public class AccelerationCap implements SpeedModifier {
 			}
 		} else {
 			// Brown-out protection
-			try {
-				double currentVoltage = pdp.getVoltageSafely();
-				if (currentVoltage < hardStopVoltage) { // If we are below hardStopVoltage, start backing off
-					outputSpeed = currentSpeed - AccelerationCap.ANTI_BROWNOUT_BACKOFF * Math.signum(currentSpeed);
-					if (Math.abs(outputSpeed) <= AccelerationCap.ANTI_BROWNOUT_BACKOFF) {
-						outputSpeed = 0;
-					}
-				} else {
+			double currentVoltage = pdp.getVoltage(); // Allow fallback to DS voltage
+			if (currentVoltage < hardStopVoltage) { // If we are below hardStopVoltage, start backing off
+				outputSpeed = currentSpeed - AccelerationCap.ANTI_BROWNOUT_BACKOFF * Math.signum(currentSpeed);
+				if (Math.abs(outputSpeed) <= AccelerationCap.ANTI_BROWNOUT_BACKOFF) {
+					outputSpeed = 0;
+				}
+			} else {
+				try {
 					double currentCurrent = pdp.getAmperage();
 					double batteryResistance = pdp.getBatteryResistanceSafely();
 					double baseVoltage = currentCurrent * batteryResistance + currentVoltage; // Battery voltage without drop due to power
@@ -112,13 +112,12 @@ public class AccelerationCap implements SpeedModifier {
 						outputSpeed = currentSpeed;
 					}
 				}
-			}
-			catch (InvalidSensorException e) { // Can't get data from PDP
-				LogKitten.ex(e);
+				catch (InvalidSensorException e) { // Can't get data from PDP
+					LogKitten.ex(e);
+				}
 			}
 			// Ramping
 			if (outputSpeed == inputSpeed) { // If we did not do any anti-brownout, just ramp
-				LogKitten.wtf("Ramping");
 				if (Math.abs(currentSpeed - inputSpeed) < AccelerationCap.MAXIMUM_MOTOR_INCREASE_PER_SECOND * deltaTime) {
 					outputSpeed = inputSpeed;
 				} else if (inputSpeed > currentSpeed) {
