6b0cd7bd87ef134a7da608550f719b38350f7fa8
Carter Turnbaugh <carturn@nuevaschool.org>
2017-02-20 21:23:59 -0800

    Added per-channel current to PDP. Also renamed to be closer to PowerDistributionPanel

--- custom/sensors/PDP.java
+++ custom/sensors/PDP.java
@@ -21,7 +21,8 @@ public class PDP {
 	protected final CustomCAN status3;
 	protected final CustomCAN statusEnergy;
 	protected double cachedVoltage;
-	protected double cachedAmperage;
+	protected double cachedCurrent;
+	protected double[] cachedChannelCurrents = new double[16];
 	protected double cachedResistance;
 	protected long lastRead;
 	private static final long MAX_AGE = 100; // How long to keep the last CAN message before throwing an error (milliseconds)
@@ -41,11 +42,50 @@ public class PDP {
 
 	/**
 	 * PDP constructor
-	 * 
+	 *
 	 * This defaults to the PDP at ID 0.
 	 */
 	public PDP() {
 		this(0);
+	}
+
+	private void readStatus1() {
+		byte[] rawArray = status1.read();
+		if (rawArray != null) {
+			cachedChannelCurrents[0] = (((rawArray[0] & 0xFF) << 2) | rawArray[1] & 0xC0) * 0.125;
+			cachedChannelCurrents[1] = (((rawArray[1] & 0x3F) << 4) | rawArray[2] & 0xF0) * 0.125;
+			cachedChannelCurrents[2] = (((rawArray[2] & 0x0F) << 6) | rawArray[3] & 0x3F) * 0.125;
+			cachedChannelCurrents[3] = (((rawArray[3] & 0xC0) << 8) | rawArray[4] & 0xFF) * 0.125;
+			cachedChannelCurrents[4] = (((rawArray[5] & 0xFF) << 2) | rawArray[6] & 0xC0) * 0.125;
+			cachedChannelCurrents[5] = (((rawArray[6] & 0x3F) << 4) | rawArray[7] & 0xF0) * 0.125;
+			lastRead = System.currentTimeMillis();
+		}
+	}
+
+	private void readStatus2() {
+		byte[] rawArray = status2.read();
+		if (rawArray != null) {
+			cachedChannelCurrents[6] = (((rawArray[0] & 0xFF) << 2) | rawArray[1] & 0xC0) * 0.125;
+			cachedChannelCurrents[7] = (((rawArray[1] & 0x3F) << 4) | rawArray[2] & 0xF0) * 0.125;
+			cachedChannelCurrents[8] = (((rawArray[2] & 0x0F) << 6) | rawArray[3] & 0x3F) * 0.125;
+			cachedChannelCurrents[9] = (((rawArray[3] & 0xC0) << 8) | rawArray[4] & 0xFF) * 0.125;
+			cachedChannelCurrents[10] = (((rawArray[5] & 0xFF) << 2) | rawArray[6] & 0xC0) * 0.125;
+			cachedChannelCurrents[11] = (((rawArray[6] & 0x3F) << 4) | rawArray[7] & 0xF0) * 0.125;
+			lastRead = System.currentTimeMillis();
+		}
+	}
+
+	private void readStatus3() {
+		byte[] rawArray = status3.read();
+		if (rawArray != null) {
+			cachedChannelCurrents[12] = (((rawArray[0] & 0xFF) << 2) | rawArray[1] & 0xC0) * 0.125;
+			cachedChannelCurrents[13] = (((rawArray[1] & 0x3F) << 4) | rawArray[2] & 0xF0) * 0.125;
+			cachedChannelCurrents[14] = (((rawArray[2] & 0x0F) << 6) | rawArray[3] & 0x3F) * 0.125;
+			cachedChannelCurrents[15] = (((rawArray[3] & 0xC0) << 8) | rawArray[4] & 0xFF) * 0.125;
+			cachedVoltage = (rawArray[6] & 0xFF) * 0.05 + 4.0;
+			cachedResistance = (rawArray[5] & 0xFF) / 1000.0; // in milliOhms
+			lastRead = System.currentTimeMillis();
+		}
 	}
 
 	/**
@@ -64,17 +104,6 @@ public class PDP {
 		catch (InvalidSensorException e) {
 			LogKitten.ex(e);
 			return DriverStation.getInstance().getBatteryVoltage();
-		}
-	}
-
-	private void readStatus3() {
-		byte[] rawArray = status3.read();
-		if (rawArray != null) {
-			int rawVoltage = rawArray[6] & 0xFF; // busVoltage is the 7th byte of the third status
-			cachedVoltage = (rawVoltage) * 0.05 + 4.0;
-			int rawResistance = rawArray[5] & 0xFF; // battery internal resistance is the 6th byte of the third status
-			cachedResistance = rawResistance / 1000.0; // in milliOhms
-			lastRead = System.currentTimeMillis();
 		}
 	}
 
@@ -113,25 +142,54 @@ public class PDP {
 	}
 
 	/**
-	 * Gets the total amperage used by the entire robot.
+	 * Gets the total current used by the entire robot.
 	 *
 	 * @return
-	 * 		Total robot amperage used.
+	 * 		Total robot current used.
 	 * @throws InvalidSensorException
 	 *         If PDP connection is lost, InvalidSensorException will be thrown.
 	 */
-	public double getAmperage() throws InvalidSensorException {
+	public double getTotalCurrent() throws InvalidSensorException {
 		byte[] rawArray = statusEnergy.read();
 		if (rawArray != null) {
 			int rawAmperage = rawArray[1] & 0xFF; // first part of current is the 2nd byte of the energy status
 			rawAmperage = rawAmperage << 4;
 			rawAmperage += (rawArray[2] & 0xF0) >> 4; // second part of current is in the 3rd byte of the energy status
-			cachedAmperage = (rawAmperage) * 0.125;
+			cachedCurrent = (rawAmperage) * 0.125;
 			lastRead = System.currentTimeMillis();
 		}
 		if (System.currentTimeMillis() - lastRead > PDP.MAX_AGE) {
 			throw new InvalidSensorException("Can not read amperage from PDP");
 		}
-		return cachedAmperage;
+		return cachedCurrent;
+	}
+
+	/**
+	 * Gets the current used by a single channel.
+	 *
+	 * @param channel
+	 *        the channel to read the current for
+	 * @return
+	 * 		the current from that channel
+	 * @throws InvalidSensorException
+	 *         if the PDP is not sending data
+	 */
+	public double getCurrent(int channel) throws InvalidSensorException {
+		if (channel < 0) {
+			return 0.0;
+		}
+		if (channel <= 5) {
+			readStatus1();
+			return cachedChannelCurrents[channel];
+		}
+		if (channel <= 11) {
+			readStatus2();
+			return cachedChannelCurrents[channel];
+		}
+		if (channel <= 15) {
+			readStatus3();
+			return cachedChannelCurrents[channel];
+		}
+		return 0.0;
 	}
 }
