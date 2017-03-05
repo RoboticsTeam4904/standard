3b9379bcccbe93b3da673d45bc7d2721bfab1601
Billy <wwpierce020602@gmail.com>
2017-03-05 11:52:29 -0800

    Fixed problems with thresholdcommand

--- commands/ThresholdCommand.java
+++ commands/ThresholdCommand.java
@@ -5,34 +5,36 @@ import java.util.function.Supplier;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- *
+ * Threshold command takes in a command, a supplier, and a threshold.
+ * When the threshold is passed by the supplier, it starts the command.
+ * When the threshold stops being passed, it cancels the command.
  */
-public class ThresholdCommand extends Command {
+public class ThresholdCommand<T extends Comparable<T>> extends Command {
 	protected final Command command;
-	protected final Supplier<Double> axis;
-	protected final double threshold;
+	protected final Supplier<T> supplier;
+	protected final T threshold;
 	protected final boolean invert;
 
-	public ThresholdCommand(Command command, Supplier<Double> axis, double threshold, boolean invert) {
+	public ThresholdCommand(Command command, Supplier<T> supplier, T threshold, boolean invert) {
 		this.command = command;
-		this.axis = axis;
+		this.supplier = supplier;
 		this.threshold = threshold;
 		this.invert = invert;
 	}
 
-	public ThresholdCommand(Command command, Supplier<Double> axis, double threshold) {
+	public ThresholdCommand(Command command, Supplier<T> axis, T threshold) {
 		this(command, axis, threshold, false);
 	}
 
-	protected boolean pastThreshold(double value) {
-		return (!invert && value >= threshold) || (invert && value <= threshold);
+	protected boolean pastThreshold() {
+		return ((supplier.get().compareTo(threshold) >= 0) ^ invert);
 	}
 
 	@Override
 	protected void execute() {
-		if (pastThreshold(axis.get()) && !command.isRunning()) {
+		if (pastThreshold() && !command.isRunning()) {
 			command.start();
-		} else if (!pastThreshold(axis.get()) && command.isRunning()) {
+		} else if (!pastThreshold() && command.isRunning()) {
 			command.cancel();
 		}
 	}
