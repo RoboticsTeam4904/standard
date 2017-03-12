76d3ff307651dd9b433ec253b00edfe1fa7081f3
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2017-03-12 16:09:28 -0700

    fixed override enable and disable

--- commands/OverrideDisable.java
+++ commands/OverrideDisable.java
@@ -4,7 +4,7 @@ package org.usfirst.frc4904.standard.commands;
 import org.usfirst.frc4904.standard.custom.Overridable;
 
 public class OverrideDisable extends OverrideSet {
-	public OverrideDisable(Overridable[] overridables) {
+	public OverrideDisable(Overridable... overridables) {
 		super(false, overridables);
 	}
 }
--- commands/OverrideEnable.java
+++ commands/OverrideEnable.java
@@ -4,7 +4,7 @@ package org.usfirst.frc4904.standard.commands;
 import org.usfirst.frc4904.standard.custom.Overridable;
 
 public class OverrideEnable extends OverrideSet {
-	public OverrideEnable(Overridable[] overridables) {
+	public OverrideEnable(Overridable... overridables) {
 		super(true, overridables);
 	}
 }
