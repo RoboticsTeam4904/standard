90b1d457138c05fc9b7fdc7e49393439cb326e99
Carter Turnbaugh <carturn@nuevaschool.org>
2017-03-26 11:04:25 -0700

    Made PIDSensor extend PIDSource

--- custom/sensors/PIDSensor.java
+++ custom/sensors/PIDSensor.java
@@ -4,36 +4,9 @@ package org.usfirst.frc4904.standard.custom.sensors;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.PIDSourceType;
 
-public interface PIDSensor {
-	/**
-	 * Set which parameter of the device you are using as a process control
-	 * variable.
-	 *
-	 * @param pidSource
-	 *        An enum to select the parameter.
-	 */
-	public void setPIDSourceType(PIDSourceType pidSource);
-
-	/**
-	 * Get which parameter of the device you are using as a process control
-	 * variable.
-	 *
-	 * @return the currently selected PID source parameter
-	 */
-	public PIDSourceType getPIDSourceType();
-
+public interface PIDSensor extends PIDSource {
 	/**
 	 * Get the result to use in PIDController
-	 * $
-	 *
-	 * @return the result to use in PIDController
-	 * @warning does not report sensor errors, will just return 0
-	 */
-	public double pidGet();
-
-	/**
-	 * Get the result to use in PIDController
-	 * $
 	 *
 	 * @return the result to use in PIDController
 	 * @throws InvalidSensorException
