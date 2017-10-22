b414db260d926cf324257614a6586149137066b6
billwpierce <wwpierce020602@gmail.com>
2017-10-21 21:32:40 -0700

    moved scaling and offset into the supplier itself

--- commands/motor/MotorSuppliedConstant.java
+++ commands/motor/MotorSuppliedConstant.java
@@ -13,8 +13,6 @@ import edu.wpi.first.wpilibj.command.Command;
 public class MotorSuppliedConstant extends Command {
 	protected final Motor motor;
 	protected final Supplier<Double> speedSupply;
-	protected final double scale;
-	protected final double offset;
 	protected double speed;
 
 	/**
@@ -24,16 +22,12 @@ public class MotorSuppliedConstant extends Command {
 	 * @param name
 	 * @param motor
 	 * @param speedSupply
-	 * @param scale
-	 * @param offset
 	 */
-	public MotorSuppliedConstant(String name, Motor motor, Supplier<Double> speedSupply, double scale, double offset) {
+	public MotorSuppliedConstant(String name, Motor motor, Supplier<Double> speedSupply) {
 		super(name);
 		this.motor = motor;
 		requires(motor);
 		this.speedSupply = speedSupply;
-		this.scale = scale;
-		this.offset = offset;
 		setInterruptible(true);
 		LogKitten.d("MotorSuppliedConstant created for " + motor.getName());
 	}
@@ -45,27 +39,14 @@ public class MotorSuppliedConstant extends Command {
 	 * @param name
 	 * @param motor
 	 * @param speedSupply
-	 * @param scale
-	 */
-	public MotorSuppliedConstant(String name, Motor motor, Supplier<Double> speedSupply, double scale) {
-		this(name, motor, speedSupply, scale, 0.0);
-	}
-
-	/**
-	 * Set the speed of a motor based on a speed supplied on initialize.
-	 * This should be used when the speed might change between construction time and when the motor should start.
-	 * 
-	 * @param name
-	 * @param motor
-	 * @param speedSupply
 	 */
 	public MotorSuppliedConstant(Motor motor, Supplier<Double> speedSupply) {
-		this("MotorSuppliedConstant", motor, speedSupply, 1.0);
+		this("MotorSuppliedConstant", motor, speedSupply);
 	}
 
 	@Override
 	protected void initialize() {
-		speed = speedSupply.get() * scale + offset;
+		speed = speedSupply.get();
 		motor.set(speed);
 	}
 
--- custom/controllers/ControllerDoubleSupplier.java
+++ custom/controllers/ControllerDoubleSupplier.java
@@ -1,11 +1,13 @@
 package org.usfirst.frc4904.standard.custom.controllers;
 
+
 import java.util.function.Supplier;
 
 public class ControllerDoubleSupplier implements Supplier<Double> {
 	protected final Controller controller;
 	protected final int axis;
 	protected final double scale;
+	protected final double offset;
 
 	/**
 	 * Wrapper for a controller that allows it to act as a double supplier for a
@@ -15,21 +17,34 @@ public class ControllerDoubleSupplier implements Supplier<Double> {
 	 * @param axis
 	 * @param scale
 	 */
-	public ControllerDoubleSupplier(Controller controller, int axis, double scale) {
+	public ControllerDoubleSupplier(Controller controller, int axis, double scale, double offset) {
 		this.controller = controller;
 		this.axis = axis;
 		this.scale = scale;
+		this.offset = offset;
 	}
 
 	/**
 	 * Wrapper for a controller that allows it to act as a double supplier for a
-	 * specific axis. Scale assumed as 1.
+	 * specific axis. Offset assumed as 0.
+	 * 
+	 * @param controller
+	 * @param axis
+	 * @param scale
+	 */
+	public ControllerDoubleSupplier(Controller controller, int axis, double scale) {
+		this(controller, axis, scale, 0.0);
+	}
+
+	/**
+	 * Wrapper for a controller that allows it to act as a double supplier for a
+	 * specific axis. Scale assumed as 1, offset as 0.
 	 * 
 	 * @param controller
 	 * @param axis
 	 */
 	public ControllerDoubleSupplier(Controller controller, int axis) {
-		this(controller, axis, 1.0);
+		this(controller, axis, 1.0, 0.0);
 	}
 
 	/**
@@ -37,6 +52,6 @@ public class ControllerDoubleSupplier implements Supplier<Double> {
 	 */
 	@Override
 	public Double get() {
-		return controller.getAxis(axis) * scale;
+		return controller.getAxis(axis) * scale + offset;
 	}
 }
