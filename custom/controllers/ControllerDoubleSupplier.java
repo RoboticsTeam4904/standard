d70a3526ffabd030c679bacd8b9bef8309b52655
billwpierce <wwpierce020602@gmail.com>
2017-10-21 21:15:39 -0700

    Added scale directly into ControllerDoubleSupplier to reduce repetition

--- custom/controllers/ControllerDoubleSupplier.java
+++ custom/controllers/ControllerDoubleSupplier.java
@@ -1,22 +1,35 @@
 package org.usfirst.frc4904.standard.custom.controllers;
 
-
 import java.util.function.Supplier;
 
 public class ControllerDoubleSupplier implements Supplier<Double> {
 	protected final Controller controller;
 	protected final int axis;
+	protected final double scale;
 
 	/**
-	 * Wrapper for a controller that allows it to act as a double supplier
-	 * for a specific axis.
+	 * Wrapper for a controller that allows it to act as a double supplier for a
+	 * specific axis set to a certain scale.
+	 * 
+	 * @param controller
+	 * @param axis
+	 * @param scale
+	 */
+	public ControllerDoubleSupplier(Controller controller, int axis, double scale) {
+		this.controller = controller;
+		this.axis = axis;
+		this.scale = scale;
+	}
+
+	/**
+	 * Wrapper for a controller that allows it to act as a double supplier for a
+	 * specific axis. Scale assumed as 1.
 	 * 
 	 * @param controller
 	 * @param axis
 	 */
 	public ControllerDoubleSupplier(Controller controller, int axis) {
-		this.controller = controller;
-		this.axis = axis;
+		this(controller, axis, 1.0);
 	}
 
 	/**
@@ -24,6 +37,6 @@ public class ControllerDoubleSupplier implements Supplier<Double> {
 	 */
 	@Override
 	public Double get() {
-		return controller.getAxis(axis);
+		return controller.getAxis(axis) * scale;
 	}
 }
