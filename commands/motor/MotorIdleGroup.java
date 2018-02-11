2020278bb8a91cb4adb1844a0a83e4d80fc80771
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2018-02-10 23:17:15 -0800

    added some more constructors

--- commands/motor/MotorIdleGroup.java
+++ commands/motor/MotorIdleGroup.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc4904.standard.commands.motor;
 
-import org.usfirst.frc4904.standard.commands.motor.MotorIdle;
+
+import org.usfirst.frc4904.standard.LogKitten;
+import org.usfirst.frc4904.standard.commands.KittenCommand;
 import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -15,16 +17,52 @@ public class MotorIdleGroup extends CommandGroup{
 	/**
 	 * Run MotorIdle (from WPILib) in parallel on every motor
 	 * from a list of motors with unrestricted size.
+	 * 
+	 * @param name
+	 * @param subsystem
+	 * @param motors
+	 */
+	public MotorIdleGroup(String name, Subsystem subsystem, boolean isDebug, Motor... motors) {
+		super(name);
+		requires(subsystem);
+		String logMessage = "";
+		for (Motor motor : motors) {
+			requires(motor);
+			addParallel(new MotorIdle(motor));
+			if (isDebug) {
+				if (motor.getName() != null) {
+					logMessage += motor.getName() + " ";
+				} else {
+					logMessage += "unnamed motor ";
+				}
+				logMessage += "idling";
+				addParallel(new KittenCommand(logMessage, LogKitten.KittenLevel.WTF));
+			}
+		}
+	}
+
+	/**
+	 * Run MotorIdle (from WPILib) in parallel on every motor
+	 * from a list of motors with unrestricted size.
+	 * 
+	 * @param name
+	 * @param subsystem
+	 * @param motors
+	 */
+	public MotorIdleGroup(String name, Subsystem subsystem, Motor... motors) {
+		this(name, subsystem, false, motors);
+	}
+
+	/**
+	 * Run MotorIdle (from WPILib) in parallel on every motor
+	 * from a list of motors with unrestricted size.
+	 * Logs the names of all motors that are being idled
 	 *
 	 * @param subsystem
 	 * @param motors
 	 */
 	public MotorIdleGroup(Subsystem subsystem, Motor... motors) {
-		super("IdleMotors");
-		requires(subsystem);
-		for (Motor motor : motors) {
-			requires(motor);
-			addParallel(new MotorIdle(motor));
-		}
+		this("Idling Motors", subsystem, motors);
 	}
 }
+
