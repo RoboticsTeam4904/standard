79c96f1c4124d0143a3006294e4e66b22447ec4d
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2019-01-20 16:10:33 -0800

    some more spark stuff

--- custom/motioncontrollers/Spark.java
+++ custom/motioncontrollers/Spark.java
@@ -7,14 +7,10 @@ import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj.SpeedController;
 
 public class Spark extends CANSparkMax implements SpeedController {
-    public final CANEncoder encoder;
-    public final CANPIDController motioncontroller;
     public Spark(int deviceNumber, CANSparkMaxLowLevel.MotorType type) {
         super(deviceNumber, type);
-        this.encoder = getEncoder();
-        this.motioncontroller = getPIDController();
-
     }
+    
     public Spark(int deviceNumber) {
         this(deviceNumber, CANSparkMaxLowLevel.MotorType.kBrushed);
     }
