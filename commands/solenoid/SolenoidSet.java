17ad9ad7fdba4993ecc1ce719cfaed9727e7f409
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2019-02-03 14:53:17 -0800

    more fixes

--- commands/solenoid/SolenoidSet.java
+++ commands/solenoid/SolenoidSet.java
@@ -1,10 +1,9 @@
 package org.usfirst.frc4904.standard.commands.solenoid;
 
+
 import org.usfirst.frc4904.standard.LogKitten;
 import org.usfirst.frc4904.standard.subsystems.SolenoidSubsystem;
 import org.usfirst.frc4904.standard.subsystems.SolenoidSubsystem.SolenoidState;
-
-
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -12,50 +11,24 @@ public class SolenoidSet extends Command {
 	protected final SolenoidSubsystem system;
 	protected final SolenoidState state;
 
-    public SolenoidSet(SolenoidSubsystem system, SolenoidState state){
+	public SolenoidSet(SolenoidSubsystem system, SolenoidState state) {
 		this.system = system;
 		this.state = state;
-        requires(system);
-        
-        
-    }
-    public void set(SolenoidState state){
-        system.setState(state);
-        LogKitten.d(state); //TODO: make a better message
-    }
-
-    @Override
-    public void execute(){    
-        switch (system.getState()){
-            case Forward: 
-                system.extendAll();
-                LogKitten.d("all solenoids extended");
-                break;
-            case Reverse:
-                system.retractAll();
-                LogKitten.d("all solenoids retracted");
-                break;
-
-            case Off: 
-                system.allOff();
-                LogKitten.d("all solenoids turned off");
-                break;
-        }
-    }
-    @Override
-	protected void interrupted() {
-        LogKitten.d("SolenoidSet interupted (solenoid state undefined)");
-        end();
+		requires(system);
 	}
-    @Override
-	protected void end() {
-		system.allOff();
-		LogKitten.d("MotorSet ended (solenoid states set to off)");
-    }
-    
-    @Override
+
+	public void set(SolenoidState state) {
+		system.setState(state);
+		LogKitten.d(state); // TODO: make a better message
+	}
+
+	@Override
+	public void initialize() {
+		system.set(state);
+	}
+
+	@Override
 	protected boolean isFinished() {
 		return false;
 	}
-
 }
--- subsystems/SolenoidSubsystem.java
+++ subsystems/SolenoidSubsystem.java
@@ -1,12 +1,14 @@
 package org.usfirst.frc4904.standard.subsystems;
 
+
 import org.usfirst.frc4904.standard.commands.solenoid.SolenoidSet;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
 public class SolenoidSubsystem extends Subsystem {
-    public enum SolenoidState {
-        OFF(DoubleSolenoid.Value.kOff), FORWARD(DoubleSolenoid.Value.kForward), REVERSE(DoubleSolenoid.Value.kReverse)
-		DoubleSolenoid.Value value;
+	public enum SolenoidState {
+		OFF(DoubleSolenoid.Value.kOff), FORWARD(DoubleSolenoid.Value.kForward), REVERSE(DoubleSolenoid.Value.kReverse);
+		public final DoubleSolenoid.Value value;
 
 		private SolenoidState(DoubleSolenoid.Value value) {
 			this.value = value;
@@ -15,10 +17,10 @@ public class SolenoidSubsystem extends Subsystem {
 	protected DoubleSolenoid[] solenoids;
 	protected SolenoidState state;
 
-    public SolenoidSubsystem(DoubleSolenoid... solenoids){
-        this.solenoids = solenoids;
-        this.state = SolenoidState.OFF;
-    }
+	public SolenoidSubsystem(DoubleSolenoid... solenoids) {
+		this.solenoids = solenoids;
+		this.state = SolenoidState.OFF;
+	}
 
 	public void setState(SolenoidState state) {
 		this.state = state;
@@ -33,27 +35,12 @@ public class SolenoidSubsystem extends Subsystem {
 			solenoid.set(state.value);
 		}
 	}
-    // public void extendAll(){
-    //     for(DoubleSolenoid solenoid:solenoids){
-    //         solenoid.set(DoubleSolenoid.Value.kForward);
-    //     }
-        
-    // }
-    // public void retractAll(){
-    //     for(DoubleSolenoid solenoid:solenoids){
-    //         solenoid.set(DoubleSolenoid.Value.kReverse);
-    //     }
-    // }
-    // public void allOff(){
-    //     for(DoubleSolenoid solenoid:solenoids){
-    //         solenoid.set(DoubleSolenoid.Value.kOff);
-    //     }
-    // }
-    public DoubleSolenoid[] getSolenoids(){
-        return solenoids;
-    }
 
-    public void initDefaultCommand(){
-        setDefaultCommand(new SolenoidSet(this, SolenoidState.FORWARD));
-    }
+	public DoubleSolenoid[] getSolenoids() {
+		return solenoids;
+	}
+
+	public void initDefaultCommand() {
+		setDefaultCommand(new SolenoidSet(this, SolenoidState.FORWARD));
+	}
 }
