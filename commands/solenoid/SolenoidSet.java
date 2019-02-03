a623f54838e0de120b94050b1ad31dd7890a9998
elkia-federation <erjung12@yahoo.com>
2019-02-03 14:24:05 -0800

    stateeee

--- commands/solenoid/SolenoidSet.java
+++ commands/solenoid/SolenoidSet.java
@@ -2,6 +2,8 @@ package org.usfirst.frc4904.standard.commands.solenoid;
 
 import org.usfirst.frc4904.standard.LogKitten;
 import org.usfirst.frc4904.standard.subsystems.SolenoidSubsystem;
+import org.usfirst.frc4904.standard.subsystems.SolenoidSubsystem.State;
+
 
 // import Solenoid.State;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
