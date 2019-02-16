ac83c5c774253bc2e8618bfe5f6e54a0b07bf810
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2019-02-16 11:48:22 -0800

    changed customXbox to use new xbox class

--- custom/controllers/CustomXbox.java
+++ custom/controllers/CustomXbox.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc4904.standard.custom.controllers;
 
 
+import edu.wpi.first.wpilibj.XboxController;
+
 /**
  * An XboxController that implements
  * the generic controller class.
@@ -11,30 +13,43 @@ public class CustomXbox extends XboxController implements Controller {
 	public final static int LEFT_Y_AXIS = 1;
 	public final static int RIGHT_X_AXIS = 2;
 	public final static int RIGHT_Y_AXIS = 3;
+	public final CustomButton a;
+	public final CustomButton b;
+	public final CustomButton x;
+	public final CustomButton y;
+	public final CustomButton lb;
+	public final CustomButton rb;
+	public final CustomButton back;
+	public final CustomButton start;
+	public final CustomButton leftStick;
+	public final CustomButton rightStick;
 
 	public CustomXbox(final int port) {
 		super(port);
+		a = new CustomButton(this, 1);
+		b = new CustomButton(this, 2);
+		x = new CustomButton(this, 3);
+		y = new CustomButton(this, 4);
+		lb = new CustomButton(this, 5);
+		rb = new CustomButton(this, 6);
+		back = new CustomButton(this, 7);
+		start = new CustomButton(this, 8);
+		leftStick = new CustomButton(this, 9);
+		rightStick = new CustomButton(this, 10);
 	}
 
-	/**
-	 * Axis 0: left joystick x
-	 * Axis 1: left joystick y
-	 * Axis 2: right joystick x
-	 * Axis 2: right joystick y
-	 */
-	@Override
 	public double getAxis(int axis) {
 		switch (axis) {
-			case 0:
-				return leftStick.getX();
-			case 1:
-				return leftStick.getY();
-			case 2:
-				return rightStick.getX();
-			case 3:
-				return rightStick.getY();
+			case LEFT_X_AXIS:
+				return getX(Hand.kLeft);
+			case LEFT_Y_AXIS:
+				return getY(Hand.kLeft);
+			case RIGHT_X_AXIS:
+				return getX(Hand.kRight);
+			case RIGHT_Y_AXIS:
+				return getY(Hand.kRight);
 			default:
-				return super.getRawAxis(axis);
+				return getRawAxis(axis);
 		}
 	}
 }
