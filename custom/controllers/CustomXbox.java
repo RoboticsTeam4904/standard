471702b989f79c0b19010682d123f956a7cfea47
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2019-02-16 12:02:17 -0800

    added additional methods

--- custom/controllers/CustomXbox.java
+++ custom/controllers/CustomXbox.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc4904.standard.custom.controllers;
 
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.XboxController;
 
 /**
@@ -13,6 +14,8 @@ public class CustomXbox extends XboxController implements Controller {
 	public final static int LEFT_Y_AXIS = 1;
 	public final static int RIGHT_X_AXIS = 2;
 	public final static int RIGHT_Y_AXIS = 3;
+	protected final double deadzone;
+	protected final int port;
 	public final CustomButton a;
 	public final CustomButton b;
 	public final CustomButton x;
@@ -24,8 +27,10 @@ public class CustomXbox extends XboxController implements Controller {
 	public final CustomButton leftStick;
 	public final CustomButton rightStick;
 
-	public CustomXbox(final int port) {
+	public CustomXbox(int port, double deadzone) {
 		super(port);
+		this.port = port;
+		this.deadzone = deadzone;
 		a = new CustomButton(this, 1);
 		b = new CustomButton(this, 2);
 		x = new CustomButton(this, 3);
@@ -38,18 +43,54 @@ public class CustomXbox extends XboxController implements Controller {
 		rightStick = new CustomButton(this, 10);
 	}
 
+	public CustomXbox(final int port) {
+		this(port, 0.0);
+	}
+
 	public double getAxis(int axis) {
 		switch (axis) {
 			case LEFT_X_AXIS:
-				return getX(Hand.kLeft);
+				return getX(Hand.kLeft) < deadzone ? 0.0 : getX(Hand.kLeft);
 			case LEFT_Y_AXIS:
-				return getY(Hand.kLeft);
+				return getY(Hand.kLeft) < deadzone ? 0.0 : getY(Hand.kLeft);
 			case RIGHT_X_AXIS:
-				return getX(Hand.kRight);
+				return getX(Hand.kRight) < deadzone ? 0.0 : getX(Hand.kRight);
 			case RIGHT_Y_AXIS:
-				return getY(Hand.kRight);
+				return getY(Hand.kRight) < deadzone ? 0.0 : getY(Hand.kRight);
 			default:
 				return getRawAxis(axis);
 		}
+	}
+
+	/**
+	 * Returns true if a given axis is above the move threshold.
+	 *
+	 * @param axis
+	 * @return
+	 */
+	public boolean active(int axis) {
+		switch (axis) {
+			case LEFT_X_AXIS:
+				return getX(Hand.kLeft) > deadzone;
+			case LEFT_Y_AXIS:
+				return getY(Hand.kLeft) > deadzone;
+			case RIGHT_X_AXIS:
+				return getX(Hand.kRight) > deadzone;
+			case RIGHT_Y_AXIS:
+				return getY(Hand.kRight) > deadzone;
+			default:
+				return false;
+		}
+	}
+
+	/**
+	 * Returns true if the xbox is actually connected.
+	 * It determines this by counting the number of buttons
+	 * (more than 0 means the xbox is connected).
+	 *
+	 * @return
+	 */
+	public boolean connected() {
+		return DriverStation.getInstance().getStickButtonCount(port) > 0;
 	}
 }
