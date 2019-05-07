cbe9d2df14ac4a1a33d6b1cfbc4cc794938dbf43
Pascal Descollonges <you@example.com>
2019-05-07 14:04:07 -0700

    cleaned up

--- subsystems/chassis/SwerveChassis.java
+++ subsystems/chassis/SwerveChassis.java
@@ -17,46 +17,44 @@ public class SwerveChassis extends Chassis {
 	 * @param wheelAngle
 	 */
 
-	public SwerveChassis(String name, SwerveModule ... modules) {
+	public SwerveChassis(String name, SwerveModule... modules) {
 		super(name);
 		this.modules = modules;
 	}
 
 	@Override
 	public void moveCartesian(double xSpeed, double ySpeed, double turnSpeed) {
-		double[][] augmentedSystem = new double [3][modules.length+1]; 
-		
-		for(int j = 0;j<augmentedSystem[0].length-1;j++){
-			augmentedSystem[0][j]=Math.sin(modules[j].getCurrentPosition());
-			augmentedSystem[1][j]=Math.cos(modules[j].getCurrentPosition());
-			augmentedSystem[2][j]=modules[j].distanceFromCenter*(sin(modules[j].angleFromCenter-modules[j].getCurrentPosition));
-		}
-		augmentedSystem[0][augmentedSystem[0].length-1]=xSpeed;
-		augmentedSystem[1][augmentedSystem[1].length-1]=ySpeed;
-		augmentedSystem[2][augmentedSystem[2].length-1]=turnSpeed;
-		double[] wheelSpeeds=minMaxUndeterminedSystem(solveSystem(augmentedSystem));
+		double[][] augmentedSystem = new double[3][modules.length + 1];
 
-		for(int i = 0; i<modules.length; i++){
+		for (int j = 0; j < augmentedSystem[0].length - 1; j++) {
+			augmentedSystem[0][j] = Math.sin(modules[j].getCurrentPosition());
+			augmentedSystem[1][j] = Math.cos(modules[j].getCurrentPosition());
+			augmentedSystem[2][j] = modules[j].distanceFromCenter
+					* (sin(modules[j].angleFromCenter - modules[j].getCurrentPosition));
+		}
+		augmentedSystem[0][augmentedSystem[0].length - 1] = xSpeed;
+		augmentedSystem[1][augmentedSystem[1].length - 1] = ySpeed;
+		augmentedSystem[2][augmentedSystem[2].length - 1] = turnSpeed;
+		double[] wheelSpeeds = minMaxUndeterminedSystem(solveSystem(augmentedSystem));
+
+		for (int i = 0; i < modules.length; i++) {
 			double xVector;
 			double yVector;
-			xVector = xSpeed + modules[i].distanceFromCenter*Math.cos(modules[i].angleFromCenter);
-			yVector = ySpeed + modules[i].distanceFromCenter*Math.sin(modules[i].angleFromCenter);
-			wheelAngle[i] = Math.atan(xVector/yVector);
-		
+			xVector = xSpeed + modules[i].distanceFromCenter * Math.cos(modules[i].angleFromCenter);
+			yVector = ySpeed + modules[i].distanceFromCenter * Math.sin(modules[i].angleFromCenter);
+			wheelAngle[i] = Math.atan(xVector / yVector);
+
 		}
 		/*
-		TODO: normalize
-		code from previous version is here:
-		frontLeftWheelSpeed /= maxSpeed;
-		frontRightWheelSpeed /= maxSpeed;
-		backLeftWheelSpeed /= maxSpeed;
-		backRightWheelSpeed /= maxSpeed;
-		*/
-		
-		for (int i = 0; i<modules.length; i++){
+		 * TODO: normalize code from previous version is here: frontLeftWheelSpeed /=
+		 * maxSpeed; frontRightWheelSpeed /= maxSpeed; backLeftWheelSpeed /= maxSpeed;
+		 * backRightWheelSpeed /= maxSpeed;
+		 */
+
+		for (int i = 0; i < modules.length; i++) {
 			modules[i].setState(wheelAngle[i], wheelSpeeds[i]);
 		}
-			
+
 		/*
 		 * for switching from cartesian to polar double totalSpeed =
 		 * Math.sqrt(Math.pow(ySpeed,2)+Math.pow(xSpeed,2)); double angle = Math.PI/2 -
@@ -74,10 +72,10 @@ public class SwerveChassis extends Chassis {
 		return wheelSpeed;
 	}
 
-	@Override 
+	@Override
 	public Motor[] getMotors() {
 		Motor[] motors = new Motor[modules.length];
-		for (int i = 0; i<modules.length; i++){
+		for (int i = 0; i < modules.length; i++) {
 			motors[i] = modules[i].linear;
 		}
 		return motors;
@@ -86,10 +84,10 @@ public class SwerveChassis extends Chassis {
 	public double[] getServoAngles() {
 		return wheelAngle;
 	}
-	
+
 	public ServoSubsystem[] getServos() {
 		ServoSubsystem[] servos = new ServoSubsystem[modules.length];
-		for (int i = 0; i<modules.length; i++){
+		for (int i = 0; i < modules.length; i++) {
 			servos[i] = modules[i].rotation;
 		}
 		return servos;
@@ -98,65 +96,76 @@ public class SwerveChassis extends Chassis {
 	public SwerveModule[] getSwerveModule() {
 		return modules;
 	}
-	
-	protected static double[][] solveSystem(double[][] augmentedSystem){ //Works at the moment 
-		for(int i=0; i<augmentedSystem.length; i++){
-			for(int j=i+1;j<augmentedSystem.length;j++){
-				double firstEquationCoefficent=augmentedSystem[j][i]/augmentedSystem[i][i];
-				for(int k=i; k<=augmentedSystem[0].length-1;k++){
-					augmentedSystem[j][k]-=augmentedSystem[i][k]*firstEquationCoefficent;
+
+	protected static double[][] solveSystem(double[][] augmentedSystem) { // Works at the moment
+		for (int i = 0; i < augmentedSystem.length; i++) {
+			for (int j = i + 1; j < augmentedSystem.length; j++) {
+				double firstEquationCoefficent = augmentedSystem[j][i] / augmentedSystem[i][i];
+				for (int k = i; k <= augmentedSystem[0].length - 1; k++) {
+					augmentedSystem[j][k] -= augmentedSystem[i][k] * firstEquationCoefficent;
 				}
 			}
 		}
-		for(int i=augmentedSystem.length-1; i>=0; i--){
-			for(int j=i-1;j>=0;j--){
-				double secondEquationCoefficent=augmentedSystem[j][i]/augmentedSystem[i][i];
-				for(int k=augmentedSystem[0].length-1; k>=0;k--){
-					augmentedSystem[j][k]-=augmentedSystem[i][k]*secondEquationCoefficent;
-					
+		for (int i = augmentedSystem.length - 1; i >= 0; i--) {
+			for (int j = i - 1; j >= 0; j--) {
+				double secondEquationCoefficent = augmentedSystem[j][i] / augmentedSystem[i][i];
+				for (int k = augmentedSystem[0].length - 1; k >= 0; k--) {
+					augmentedSystem[j][k] -= augmentedSystem[i][k] * secondEquationCoefficent;
+
 				}
 			}
 		}
 		return augmentedSystem;
 	}
-	protected static double[] minMaxUndeterminedSystem(double[][] solvedMatrix){ //Untested
-		double[] intersectionX=new double[(solvedMatrix.length*(solvedMatrix.length+1))/2];
-		int k=0;
-		for(int i=0; i<solvedMatrix.length-1; i++){
-			for(int j=i+1; j<solvedMatrix.length;j++){
-				double determinant=solvedMatrix[i][i]*solvedMatrix[j][solvedMatrix[j].length-2]-
-								   solvedMatrix[j][j]*solvedMatrix[i][solvedMatrix[i].length-2];
-				if(determinant != 0){
-					intersectionX[k]=(solvedMatrix[i][solvedMatrix[j].length-1]*solvedMatrix[j][solvedMatrix[j].length-2]-
-									  solvedMatrix[j][solvedMatrix[j].length-1]*solvedMatrix[i][solvedMatrix[i].length-2])/determinant;
-					intersectionX[k+1]=(-solvedMatrix[i][solvedMatrix[j].length-1]*solvedMatrix[j][solvedMatrix[j].length-2]-
-									  solvedMatrix[j][solvedMatrix[j].length-1]*-solvedMatrix[i][solvedMatrix[i].length-2])/determinant;
-					intersectionX[k+2]=(solvedMatrix[i][solvedMatrix[j].length-1]*solvedMatrix[j][solvedMatrix[j].length-2]+
-									  solvedMatrix[j][solvedMatrix[j].length-1]*solvedMatrix[i][solvedMatrix[i].length-2])/determinant;	
-					intersectionX[k+3]=(-solvedMatrix[i][solvedMatrix[j].length-1]*solvedMatrix[j][solvedMatrix[j].length-2]+
-									  solvedMatrix[j][solvedMatrix[j].length-1]*solvedMatrix[i][solvedMatrix[i].length-2])/determinant;					  
+
+	protected static double[] minMaxUndeterminedSystem(double[][] solvedMatrix) { // Untested
+		double[] intersectionX = new double[(solvedMatrix.length * (solvedMatrix.length + 1)) / 2];
+		int k = 0;
+		for (int i = 0; i < solvedMatrix.length - 1; i++) {
+			for (int j = i + 1; j < solvedMatrix.length; j++) {
+				double determinant = solvedMatrix[i][i] * solvedMatrix[j][solvedMatrix[j].length - 2]
+						- solvedMatrix[j][j] * solvedMatrix[i][solvedMatrix[i].length - 2];
+				if (determinant != 0) {
+					intersectionX[k] = (solvedMatrix[i][solvedMatrix[j].length - 1]
+							* solvedMatrix[j][solvedMatrix[j].length - 2]
+							- solvedMatrix[j][solvedMatrix[j].length - 1] * solvedMatrix[i][solvedMatrix[i].length - 2])
+							/ determinant;
+					intersectionX[k + 1] = (-solvedMatrix[i][solvedMatrix[j].length - 1]
+							* solvedMatrix[j][solvedMatrix[j].length - 2]
+							- solvedMatrix[j][solvedMatrix[j].length - 1]
+									* -solvedMatrix[i][solvedMatrix[i].length - 2])
+							/ determinant;
+					intersectionX[k + 2] = (solvedMatrix[i][solvedMatrix[j].length - 1]
+							* solvedMatrix[j][solvedMatrix[j].length - 2]
+							+ solvedMatrix[j][solvedMatrix[j].length - 1] * solvedMatrix[i][solvedMatrix[i].length - 2])
+							/ determinant;
+					intersectionX[k + 3] = (-solvedMatrix[i][solvedMatrix[j].length - 1]
+							* solvedMatrix[j][solvedMatrix[j].length - 2]
+							+ solvedMatrix[j][solvedMatrix[j].length - 1] * solvedMatrix[i][solvedMatrix[i].length - 2])
+							/ determinant;
+				} else {
+					intersectionX[k] = Double.NaN;
 				}
-				else{
-					intersectionX[k]= Double.NaN;
-				}
-				k+=4;
+				k += 4;
 			}
 		}
-		double minMaxValue= Double.POSITIVE_INFINITY;
-		double[] bestValues= new double[solvedMatrix.length+1];
-		double[] currentValues= new double[solvedMatrix.length+1];
-		
-		for(int i=0; i<intersectionX.length; i++){
-			for(int j=0; j<solvedMatrix.length;j++){
-				currentValues[j]=(solvedMatrix[j][solvedMatrix.length-1]-solvedMatrix[j][solvedMatrix.length-2]*intersectionX[i])/solvedMatrix[j][j];
+		double minMaxValue = Double.POSITIVE_INFINITY;
+		double[] bestValues = new double[solvedMatrix.length + 1];
+		double[] currentValues = new double[solvedMatrix.length + 1];
+
+		for (int i = 0; i < intersectionX.length; i++) {
+			for (int j = 0; j < solvedMatrix.length; j++) {
+				currentValues[j] = (solvedMatrix[j][solvedMatrix.length - 1]
+						- solvedMatrix[j][solvedMatrix.length - 2] * intersectionX[i]) / solvedMatrix[j][j];
 			}
-			if(Math.max(Math.max(Math.abs(currentValues[0]),Math.abs(currentValues[1])),Math.max(Math.abs(currentValues[2]),Math.abs(currentValues[3])))< minMaxValue){
-				minMaxValue= Math.max(Math.max(Math.abs(currentValues[0]),Math.abs(currentValues[1])), Math.max(Math.abs(currentValues[2]),Math.abs(currentValues[3])));
-				bestValues=currentValues;
+			if (Math.max(Math.max(Math.abs(currentValues[0]), Math.abs(currentValues[1])),
+					Math.max(Math.abs(currentValues[2]), Math.abs(currentValues[3]))) < minMaxValue) {
+				minMaxValue = Math.max(Math.max(Math.abs(currentValues[0]), Math.abs(currentValues[1])),
+						Math.max(Math.abs(currentValues[2]), Math.abs(currentValues[3])));
+				bestValues = currentValues;
 			}
 		}
 		return bestValues;
 	}
 
 }
-	
--- subsystems/motor/SwerveModule.java
+++ subsystems/motor/SwerveModule.java
@@ -3,55 +3,55 @@ package org.usfirst.frc4904.standard.subsystems.motor;
 import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 import org.usfirst.frc4904.standard.subsystems.motor.ServoSubsystem;
 
-
 public class SwerveModule {
-		public final Motor linear;
-		public final ServoSubsystem rotation;
-		public double angleFromCenter;
-		public double distanceFromCenter;
-		public double wheelSpeed;
-		public double wheelAngle;
+	public final Motor linear;
+	public final ServoSubsystem rotation;
+	public double angleFromCenter;
+	public double distanceFromCenter;
+	public double wheelSpeed;
+	public double wheelAngle;
 
+	/**
+	 * Constructs a single swerve module
+	 * 
+	 * @param linear   must have a range of at least 180 degrees
+	 * @param rotation
+	 * @param angle    (unit in degrees)
+	 * @param distance
+	 */
 
-		/**
-		 * Constructs a single swerve module
-		 * 
-		 * @param linear must have a range of at least 180 degrees
-		 * @param rotation
-         * @param angle (unit in degrees)
-         * @param distance
-		 */
-
-		public SwerveModule(Motor linear, ServoSubsystem rotation, 
-							double angleFromCenter, double distanceFromCenter) {
-			this.linear = linear;
-            this.rotation = rotation;
-            this.angleFromCenter = angleFromCenter;
-			this.distanceFromCenter = distanceFromCenter;
-		}
-
-		public void setState(double speed, double angle) {
-			this.setAngle(angle);
-			this.setSpeed(speed);	
-
-		}
-		public void setAngle(double angle){
-			if (angle >= 180) { // TODO: radians
-				this.wheelSpeed *= -1;
-				this.wheelAngle = angle - 180;
-			} else {
-				this.wheelAngle = angle;
-			}
-		}
-		public void setSpeed(double speed){
-			this.wheelSpeed = speed;
-		}
-		
-		public double getCurrentPosition(){
-			return rotation.getCurrentPosition();
-		}
-
-			// Osher's comment: I don't know if you'd be able to just set any angle to the servo. 
-			// Maybe set it incrementally?
-			// Should end up actually calling the motors
+	public SwerveModule(Motor linear, ServoSubsystem rotation, double angleFromCenter, double distanceFromCenter) {
+		this.linear = linear;
+		this.rotation = rotation;
+		this.angleFromCenter = angleFromCenter;
+		this.distanceFromCenter = distanceFromCenter;
 	}
+
+	public void setState(double speed, double angle) {
+		this.setAngle(angle);
+		this.setSpeed(speed);
+
+	}
+
+	public void setAngle(double angle) {
+		if (angle >= 180) { // TODO: radians
+			this.wheelSpeed *= -1;
+			this.wheelAngle = angle - 180;
+		} else {
+			this.wheelAngle = angle;
+		}
+	}
+
+	public void setSpeed(double speed) {
+		this.wheelSpeed = speed; 
+	}
+
+	public double getCurrentPosition() {
+		return rotation.getCurrentPosition();
+	}
+
+	// Osher's comment: I don't know if you'd be able to just set any angle to the
+	// servo.
+	// Maybe set it incrementally?
+	// Should end up actually calling the motors
+}
