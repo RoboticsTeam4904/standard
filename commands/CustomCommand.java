33d3c13bcc6d294185e1aba412fbe3dbc5e04982
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2020-01-08 16:05:59 -0800

    making custom command

---
+++ commands/CustomCommand.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc4904.standard.commands;
+
+import java.util.Set;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+
+public abstract class CustomCommand extends CommandBase {
+    protected double timeout = 0.0;
+
+    
+    public CustomCommand(String name, double timeout, Subsystem... requirements) {
+        this(name, requirements);
+        this.withTimeout(timeout);
+    }
+
+    public CustomCommand(String name, Subsystem... requirements) {
+        super();
+        setName(name);
+        addRequirements(requirements);
+    }
+
+    //TODO: write this
+    public boolean isRunning() {
+        return true;
+    }
+
+
+
+}
