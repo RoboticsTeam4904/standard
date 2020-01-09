6a0d14cc47d514d1aefa047e0c9e45ae242ebf11
Exr0n <25516241+Exr0n@users.noreply.github.com>
2020-01-08 17:23:59 -0800

    fix to use CustomCommand

--- commands/Cancel.java
+++ commands/Cancel.java
@@ -3,16 +3,18 @@ package org.usfirst.frc4904.standard.commands;
 import java.util.Set;
 
 import org.usfirst.frc4904.standard.LogKitten;
+import org.usfirst.frc4904.standard.commands.CustomCommand;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 /**
  * This class cancels a command
  */
-public class Cancel implements Command {
+public class Cancel extends CustomCommand {
 	protected final Command command;
 
 	public Cancel(Command command) {
+		super("Cancel " + command);
 		this.command = command;
 	}
 
