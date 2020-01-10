1077c15145a5a85526ee77917720942155f06573
Exr0n <spotyie@gmail.com>
2020-01-09 16:56:28 -0800

    update InjectedCommand to use CommandBase

--- commands/InjectedCommand.java
+++ commands/InjectedCommand.java
@@ -3,29 +3,29 @@ package org.usfirst.frc4904.standard.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public abstract class InjectedCommand extends CommandBase {
-	private final CustomCommand previous;
-	
-	public InjectedCommand(String name, double timeout, CustomCommand previous) {
+	private final CommandBase previous;
+
+	public InjectedCommand(String name, double timeout, CommandBase previous) {
 		super();
 		this.withTimeout(timeout);
 		setName(name);
 		this.previous = previous;
 	}
 
-	public InjectedCommand(double timeout, CustomCommand previous) {
+	public InjectedCommand(double timeout, CommandBase previous) {
 		this("Injected(" + previous.getName() + ")", timeout, previous);
 	}
 
-	public InjectedCommand(String name, CustomCommand previous) {
+	public InjectedCommand(String name, CommandBase previous) {
 		super();
 		setName(name);
 		this.previous = previous;
 	}
 
-	public InjectedCommand(CustomCommand previous) {
+	public InjectedCommand(CommandBase previous) {
 		this("Injected(" + previous.getName() + ")", previous);
 	}
-	
+
 	@Override
 	final public void initialize() {
 		if (previous != null && (previous.isScheduled())) {
@@ -33,24 +33,24 @@ public abstract class InjectedCommand extends CommandBase {
 		}
 		onInitialize();
 	}
-	
+
 	final public void interrupted() {
 		onInterrupted();
 		if (previous != null && (!previous.isScheduled())) {
 			previous.schedule();
 		}
 	}
-	
+
 	final public void end() {
 		onEnd();
 		if (previous != null && (!previous.isScheduled())) {
 			previous.schedule();
 		}
 	}
-	
+
 	abstract protected void onInitialize();
-	
+
 	abstract protected void onInterrupted();
-	
+
 	abstract protected void onEnd();
 }
