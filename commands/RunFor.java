f1b7e122d8efec372becc00ad8bc5998afe0f198
Exr0n <spotyie@gmail.com>
2020-01-09 17:48:06 -0800

    create RunFor named constructor

--- commands/RunFor.java
+++ commands/RunFor.java
@@ -4,10 +4,27 @@ import java.util.Set;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class RunFor implements CommandBase {
+public class RunFor extends CommandBase {
 	protected final double duration;
 	protected final CommandBase command;
 	protected boolean firstTick;
+
+	/**
+	 * Run a command for a given amount of time, in seconds. The command will be
+	 * cancelled at the end. For example, if you want to go forward for 3 seconds,
+	 * use: new RunFor(new GoForward(), 3)
+	 *
+	 * @param name          The name of this RunFor
+	 * @param command       The command to be run for the duration
+	 * @param duration      A duration in seconds
+	 * @param interruptible Whether this command should be interruptible
+	 */
+	public RunFor(String name, CommandBase command, double duration) {
+		setName(name);
+		this.duration = duration;
+		this.command = command;
+		firstTick = true;
+	}
 
 	/**
 	 * Run a command for a given amount of time, in seconds. The command will be
@@ -19,9 +36,7 @@ public class RunFor implements CommandBase {
 	 * @param interruptible Whether this command should be interruptible
 	 */
 	public RunFor(CommandBase command, double duration) {
-		this.duration = duration;
-		this.command = command;
-		firstTick = true;
+		this("RunFor[" + command.getName() + "]", command, duration);
 	}
 
 	public void initialize() {
