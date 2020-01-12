752919422b795aa221ba7fd860982268d332a954
Evan Steirman <evastei@nus12369-11-evastei.nueva.local>
2020-01-12 11:35:57 -0800

    fixed PR comments

--- commands/Cancel.java
+++ commands/Cancel.java
@@ -1,19 +1,15 @@
 package org.usfirst.frc4904.standard.commands;
 
-import java.util.Set;
-
 import org.usfirst.frc4904.standard.LogKitten;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 
 /**
  * This class cancels a command
  */
 public class Cancel extends CommandBase {
-	protected final Command command;
+	protected final CommandBase command;
 
-	public Cancel(Command command) {
+	public Cancel(CommandBase command) {
 		super();
 		setName("Cancel " + command.getName());
 		this.command = command;
@@ -26,9 +22,5 @@ public class Cancel extends CommandBase {
 
 	public boolean isFinished() {
 		return true;
-	}
-
-	public Set<Subsystem> getRequirements() {
-		return Set.of();
 	}
 }
