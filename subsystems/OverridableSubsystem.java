a8ae11554dc6e950b808da3ece88c8610abc3463
bcho04 <brandon.cho@gmail.com>
2020-01-12 10:28:49 -0800

    Make requested changes

--- subsystems/OverridableSubsystem.java
+++ subsystems/OverridableSubsystem.java
@@ -2,10 +2,14 @@ package org.usfirst.frc4904.standard.subsystems;
 
 
 import org.usfirst.frc4904.standard.custom.Overridable;
-import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public abstract class OverridableSubsystem implements Subsystem, Overridable {
+public abstract class OverridableSubsystem extends SubsystemBase implements Overridable {
 	private boolean isOverridden = false;
+
+	public OverridableSubsystem(String name) {
+		setName(name);
+	}
 
 	public void setOverridden(boolean isOverridden) {
 		this.isOverridden = isOverridden;
