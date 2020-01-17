ba76c383553a17fc48097196f425f1148ee36399
Daniel Bernstein <danbern@nuevaschool.org>
2020-01-16 16:51:02 -0800

    noop update

--- commands/Noop.java
+++ commands/Noop.java
@@ -3,12 +3,14 @@ package org.usfirst.frc4904.standard.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class Noop extends CommandBase {
+	public Noop() {	
+		super();	
+		setName("NOOP");
+	}
+
 	@Override
 	public boolean isFinished() {
 		return true;
 	}
 
-	@Override
-	public void end(boolean wasInterrupted) {
-	}
 }
