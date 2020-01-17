1a260e59a9c8353def06f3e60a9d399aeb95e7ac
Exr0n <spotyie@gmail.com>
2020-01-16 17:57:32 -0800

    update MotorBrake.java for 2020-update

--- commands/motor/MotorBrake.java
+++ commands/motor/MotorBrake.java
@@ -2,19 +2,20 @@ package org.usfirst.frc4904.standard.commands.motor;
 
 import org.usfirst.frc4904.standard.subsystems.motor.BrakeableMotor;
 
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class MotorBrake extends Command {
+public class MotorBrake extends CommandBase {
 	protected final BrakeableMotor[] motors;
 
 	public MotorBrake(String name, BrakeableMotor... motors) {
-		super(name);
+		super();
+		setName(name);
 		this.motors = motors;
 		for (BrakeableMotor motor : motors) {
 			motor.setBrakeMode();
-			if (motor instanceof Subsystem) {
-				requires((Subsystem) motor);
+			if (motor instanceof SubsystemBase) {
+				addRequirements((SubsystemBase) motor);
 			}
 		}
 	}
