2fb20baaa47bc4d8ac378c957c55132ab668ce88
Exr0n <spotyie@gmail.com>
2020-01-16 17:29:30 -0800

    format SolenoidExtend

--- commands/solenoid/SolenoidExtend.java
+++ commands/solenoid/SolenoidExtend.java
@@ -27,9 +27,9 @@ public class SolenoidExtend extends SolenoidSet {
 	 * Command to set the state of a SolenoidSubsystem to
 	 * EXTEND(DoubleSolenoid.Value.kForward)
 	 * 
-	 * @param system SolenoidSubsystem to set 
-	 * @param booleanSuppliers conditions that if true, prevent the setting of the 
-	 * 						   system
+	 * @param system           SolenoidSubsystem to set
+	 * @param booleanSuppliers conditions that if true, prevent the setting of the
+	 *                         system
 	 */
 	public SolenoidExtend(SolenoidSubsystem system, BooleanSupplier... booleanSuppliers) {
 		this("Extending " + system.getName(), system, booleanSuppliers);
