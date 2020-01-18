cc01173b4cec7038b938254e185c104f41e30d6a
Daniel Bernstein <danbern@nuevaschool.org>
2020-01-17 18:48:06 -0800

    update missing ones and delete deprecated

--- commands/Kill.java
+++ commands/Kill.java
@@ -1,22 +1,18 @@
 package org.usfirst.frc4904.standard.commands;
 
-
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 /**
- * This command is designed to allow
- * a driver or operator to kill a set
- * of subsystems. All it does is
- * trigger all of the commands in
- * parallel.
+ * This command is designed to allow a driver or operator to kill a set of
+ * subsystems. All it does is trigger all of the commands in parallel.
  */
-public class Kill extends CommandGroup {
-	public Kill(Command... kills) {
-		super("Kill");
-		setInterruptible(false); // should kill robot forever
-		for (Command kill : kills) {
-			addParallel(kill);
+public class Kill extends ParallelCommandGroup {
+	public Kill(CommandBase... kills) {
+		super();
+		setName("Kill");
+		for (CommandBase kill : kills) {
+			addCommands(kill);
 		}
 	}
 }
