b380e4eda88fb8af598ecf16fc34ca2fec9e760f
Billy Pierce <bilpier@nuevaschool.org>
2020-01-22 17:35:55 -0800

    cleaned up and compartmentalized SimpleSplines, while adding new constructors

--- commands/chassis/SimpleSplines.java
+++ commands/chassis/SimpleSplines.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.standard.subsystems.chassis.SensorDrive;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.RamseteController;
@@ -21,30 +22,46 @@ import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class SimpleSplines extends SequentialCommandGroup {
-  public SimpleSplines(SensorDrive robotDrive, Pose2d init_pos, List<Translation2d> inter_points, Pose2d final_pos, AutoConstants autoConstants, DriveConstants driveConstants){
+  public SimpleSplines(SensorDrive robotDrive, Pose2d init_pos, List<Translation2d> inter_points, Pose2d final_pos, double maxVoltage, Command nextCommand){
     super(new RamseteCommand(
-        TrajectoryGenerator.generateTrajectory(init_pos, inter_points, final_pos, new TrajectoryConfig(autoConstants.kMaxSpeedMetersPerSecond,
-        autoConstants.kMaxAccelerationMetersPerSecondSquared)
-            .setKinematics(driveConstants.kDriveKinematics)
+        TrajectoryGenerator.generateTrajectory(init_pos, inter_points, final_pos, new TrajectoryConfig(robotDrive.getAutoConstants().kMaxSpeedMetersPerSecond,
+        robotDrive.getAutoConstants().kMaxAccelerationMetersPerSecondSquared)
+            .setKinematics(robotDrive.getDriveConstants().kDriveKinematics)
             .addConstraint(new DifferentialDriveVoltageConstraint(
-              new SimpleMotorFeedforward(driveConstants.ksVolts, 
-              driveConstants.kvVoltSecondsPerMeter, 
-              driveConstants.kaVoltSecondsSquaredPerMeter), 
-              driveConstants.kDriveKinematics, 
-              10))),
+              new SimpleMotorFeedforward(robotDrive.getDriveConstants().ksVolts, 
+              robotDrive.getDriveConstants().kvVoltSecondsPerMeter, 
+              robotDrive.getDriveConstants().kaVoltSecondsSquaredPerMeter), 
+              robotDrive.getDriveConstants().kDriveKinematics, 
+              maxVoltage))),
         robotDrive::getPose,
-        new RamseteController(autoConstants.kRamseteB, autoConstants.kRamseteZeta),
-        new SimpleMotorFeedforward(driveConstants.ksVolts,
-        driveConstants.kvVoltSecondsPerMeter,
-        driveConstants.kaVoltSecondsSquaredPerMeter),
-        driveConstants.kDriveKinematics,
+        new RamseteController(robotDrive.getAutoConstants().kRamseteB, robotDrive.getAutoConstants().kRamseteZeta),
+        new SimpleMotorFeedforward(robotDrive.getDriveConstants().ksVolts,
+        robotDrive.getDriveConstants().kvVoltSecondsPerMeter,
+        robotDrive.getDriveConstants().kaVoltSecondsSquaredPerMeter),
+        robotDrive.getDriveConstants().kDriveKinematics,
         robotDrive::getWheelSpeeds,
-        new PIDController(driveConstants.kPDriveVel, 0, 0),
-        new PIDController(driveConstants.kPDriveVel, 0, 0),
+        new PIDController(robotDrive.getDriveConstants().kPDriveVel, 0, 0),
+        new PIDController(robotDrive.getDriveConstants().kPDriveVel, 0, 0),
         // RamseteCommand passes volts to the callback
-        robotDrive::tankDriveVolts, robotDrive), new InstantCommand(() -> robotDrive.tankDriveVolts(0, 0)));
+        robotDrive::tankDriveVolts, robotDrive), nextCommand);
+  } 
+
+  public SimpleSplines(SensorDrive robotDrive, Pose2d init_pos, List<Translation2d> inter_points, Pose2d final_pos, double maxVoltage){
+    this(robotDrive, init_pos, inter_points, final_pos, maxVoltage, new InstantCommand(() -> robotDrive.tankDriveVolts(0, 0)));
   }
 
+  public SimpleSplines(SensorDrive robotDrive, Pose2d init_pos, List<Translation2d> inter_points, Pose2d final_pos){
+    this(robotDrive, init_pos, inter_points, final_pos, 10);
+  }
+
+  public SimpleSplines(SensorDrive robotDrive, Pose2d init_pos, Pose2d final_pos){
+    this(robotDrive, init_pos, List.of(), final_pos);
+  }
+
+
+  /**
+   * Class to store autonomous constants used for Ramsete Pathing.
+   */
   public static class AutoConstants {
     public double kMaxSpeedMetersPerSecond;
 		public double kMaxAccelerationMetersPerSecondSquared;
@@ -59,20 +76,23 @@ public class SimpleSplines extends SequentialCommandGroup {
     }
   }
 
+  /**
+   * Class to store drive constants used for Ramsete pathing.
+   */
   public static class DriveConstants {
-    public double ksVolts = 0.319;
-		public double kvVoltSecondsPerMeter = 4.71;
-    public double kaVoltSecondsSquaredPerMeter = 0.208;
-    public double kTrackwidthMeters = 0.6161858257789627; //0.5842
-		public DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(kTrackwidthMeters);;
-    public double kPDriveVel = 7.28;
+    public double ksVolts;
+		public double kvVoltSecondsPerMeter;
+    public double kaVoltSecondsSquaredPerMeter;
+    public double kTrackwidthMeters;
+		public DifferentialDriveKinematics kDriveKinematics;
+    public double kPDriveVel;
     
-    public DriveConstants(double ksVolts, double kvVoltSecondsPerMeter, double kaVoltSecondsSquaredPerMeter, double kTrackwidthMeters, DifferentialDriveKinematics kDriveKinematics, double kPDriveVel){
+    public DriveConstants(double ksVolts, double kvVoltSecondsPerMeter, double kaVoltSecondsSquaredPerMeter, double kTrackwidthMeters, double kPDriveVel){
       this.ksVolts = ksVolts;
       this.kvVoltSecondsPerMeter = kvVoltSecondsPerMeter;
       this.kaVoltSecondsSquaredPerMeter = kaVoltSecondsSquaredPerMeter;
       this.kTrackwidthMeters = kTrackwidthMeters;
-      this.kDriveKinematics = kDriveKinematics;
+      this.kDriveKinematics = new DifferentialDriveKinematics(kTrackwidthMeters);
       this.kPDriveVel = kPDriveVel;
     }
   }
