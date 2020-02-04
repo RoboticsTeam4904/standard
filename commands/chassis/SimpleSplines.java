2f7cd2cf0a3931f0cde5a525ba05cc17313aa449
Billy Pierce <bilpier@nuevaschool.org>
2020-02-03 16:56:33 -0800

    fixed simplesplines and sensordrive

--- commands/chassis/SimpleSplines.java
+++ commands/chassis/SimpleSplines.java
@@ -1,65 +1,75 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+// Based on software from FIRST and WPI
 
 package org.usfirst.frc4904.standard.commands.chassis;
 
-import java.util.List;
-
-import org.usfirst.frc4904.robot.RobotMap;
-import org.usfirst.frc4904.robot.RobotMap.DriveConstants;
-import org.usfirst.frc4904.standard.LogKitten;
 import org.usfirst.frc4904.standard.subsystems.chassis.SensorDrive;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.controller.RamseteController;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
-import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
-import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class SimpleSplines extends SequentialCommandGroup {
-  // Create a voltage constraint to ensure we don't accelerate too fast
-  public static final DifferentialDriveVoltageConstraint autoVoltageConstraint = 
-    new DifferentialDriveVoltageConstraint(
-      new SimpleMotorFeedforward(RobotMap.DriveConstants.ksVolts, 
-      RobotMap.DriveConstants.kvVoltSecondsPerMeter, 
-      RobotMap.DriveConstants.kaVoltSecondsSquaredPerMeter), 
-      RobotMap.DriveConstants.kDriveKinematics, 
-      10);
-  public static final TrajectoryConfig config =
-        new TrajectoryConfig(RobotMap.AutoConstants.kMaxSpeedMetersPerSecond,
-        RobotMap.AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-            // Add kinematics to ensure max speed is actually obeyed
-            .setKinematics(RobotMap.DriveConstants.kDriveKinematics)
-            // Apply the voltage constraint
-            .addConstraint(autoVoltageConstraint);
-
-  public SimpleSplines(SensorDrive robotDrive, Pose2d init_pos, Pose2d final_pos, List inter_points){
+  public SimpleSplines(SensorDrive robotDrive, Trajectory trajectory, Command nextCommand){
     super(new RamseteCommand(
-        TrajectoryGenerator.generateTrajectory(init_pos, inter_points, final_pos, config),
+        trajectory,
         robotDrive::getPose,
-        new RamseteController(RobotMap.AutoConstants.kRamseteB, RobotMap.AutoConstants.kRamseteZeta),
-        new SimpleMotorFeedforward(RobotMap.DriveConstants.ksVolts,
-        RobotMap.DriveConstants.kvVoltSecondsPerMeter,
-        RobotMap.DriveConstants.kaVoltSecondsSquaredPerMeter),
-        RobotMap.DriveConstants.kDriveKinematics,
+        new RamseteController(robotDrive.getAutoConstants().kRamseteB, robotDrive.getAutoConstants().kRamseteZeta),
+        new SimpleMotorFeedforward(robotDrive.getDriveConstants().ksVolts,
+        robotDrive.getDriveConstants().kvVoltSecondsPerMeter,
+        robotDrive.getDriveConstants().kaVoltSecondsSquaredPerMeter),
+        robotDrive.getDriveConstants().kDriveKinematics,
         robotDrive::getWheelSpeeds,
-        new PIDController(RobotMap.DriveConstants.kPDriveVel, 0, 0),
-        new PIDController(RobotMap.DriveConstants.kPDriveVel, 0, 0),
+        new PIDController(robotDrive.getDriveConstants().kPDriveVel, 0, 0),
+        new PIDController(robotDrive.getDriveConstants().kPDriveVel, 0, 0),
         // RamseteCommand passes volts to the callback
-        robotDrive::tankDriveVolts,
-        robotDrive.getDriveBase().getMotors()
-    ), new InstantCommand(() -> robotDrive.tankDriveVolts(0, 0)));
+        robotDrive::tankDriveVolts, robotDrive.getDriveBase().getMotors()), nextCommand);
+  } 
+
+  public SimpleSplines(SensorDrive robotDrive, Trajectory trajectory){
+    this(robotDrive, trajectory, new InstantCommand(() -> robotDrive.tankDriveVolts(0, 0)));
+  }
+
+  /**
+   * Class to store autonomous constants used for Ramsete Pathing.
+   */
+  public static class SplineAutoConstants {
+    public double kMaxSpeedMetersPerSecond;
+		public double kMaxAccelerationMetersPerSecondSquared;
+		public double kRamseteB;
+		public double kRamseteZeta;
+
+    public SplineAutoConstants(double kMaxSpeedMetersPerSecond, double kMaxAccelerationMetersPerSecondSquared, double kRamseteB, double kRamseteZeta) {
+      this.kMaxSpeedMetersPerSecond = kMaxSpeedMetersPerSecond;
+      this.kMaxAccelerationMetersPerSecondSquared = kMaxAccelerationMetersPerSecondSquared;
+      this.kRamseteB = kRamseteB;
+      this.kRamseteZeta = kRamseteZeta;
+    }
+  }
+
+  /**
+   * Class to store drive constants used for Ramsete pathing.
+   */
+  public static class SplineDriveConstants {
+    public double ksVolts;
+		public double kvVoltSecondsPerMeter;
+    public double kaVoltSecondsSquaredPerMeter;
+    public double kTrackwidthMeters;
+		public DifferentialDriveKinematics kDriveKinematics;
+    public double kPDriveVel;
+    
+    public SplineDriveConstants(double ksVolts, double kvVoltSecondsPerMeter, double kaVoltSecondsSquaredPerMeter, double kTrackwidthMeters, double kPDriveVel){
+      this.ksVolts = ksVolts;
+      this.kvVoltSecondsPerMeter = kvVoltSecondsPerMeter;
+      this.kaVoltSecondsSquaredPerMeter = kaVoltSecondsSquaredPerMeter;
+      this.kTrackwidthMeters = kTrackwidthMeters;
+      this.kDriveKinematics = new DifferentialDriveKinematics(kTrackwidthMeters);
+      this.kPDriveVel = kPDriveVel;
+    }
   }
 }
--- subsystems/chassis/SensorDrive.java
+++ subsystems/chassis/SensorDrive.java
@@ -7,42 +7,71 @@
 
 package org.usfirst.frc4904.standard.subsystems.chassis;
 
+import java.util.List;
+
 import com.ctre.phoenix.sensors.CANCoder;
 
+import org.usfirst.frc4904.standard.commands.chassis.SimpleSplines;
 import org.usfirst.frc4904.standard.custom.sensors.IMU;
 import org.usfirst.frc4904.standard.subsystems.chassis.TankDrive;
 import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.spline.Spline;
+import edu.wpi.first.wpilibj.spline.SplineHelper;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryConfig;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator.ControlVectorList;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
-public class SensorDrive implements Subsystem { // Based largely on https://github.com/wpilibsuite/allwpilib/blob/master/wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/ramsetecommand/subsystems/DriveSubsystem.java
+public class SensorDrive implements Subsystem { // Based largely on
+                                                // https://github.com/wpilibsuite/allwpilib/blob/master/wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/ramsetecommand/subsystems/DriveSubsystem.java
   private final TankDrive driveBase;
   private final CANCoder leftEncoder;
   private final CANCoder rightEncoder;
+  private final SimpleSplines.SplineAutoConstants autoConstants;
+  private final SimpleSplines.SplineDriveConstants driveConstants;
   private final IMU gyro;
   private final DifferentialDriveOdometry odometry;
+  private TrajectoryConfig pathConfig;
+
   /**
    * Creates a new DriveSubsystem.
    */
-  public SensorDrive(TankDrive driveBase, CANCoder leftEncoder, CANCoder rightEncoder, IMU gyro) {
+  public SensorDrive(TankDrive driveBase, SimpleSplines.SplineAutoConstants autoConstants, SimpleSplines.SplineDriveConstants driveConstants, CANCoder leftEncoder, CANCoder rightEncoder, IMU gyro) {
     this.driveBase = driveBase;
     this.leftEncoder = leftEncoder;
     this.rightEncoder = rightEncoder;
+    this.autoConstants = autoConstants;
+    this.driveConstants = driveConstants;
     this.gyro = gyro;
 
     resetEncoders();
     odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(getHeading()));
+    configuratePath(10);
     CommandScheduler.getInstance().registerSubsystem(this);
   }
 
   @Override
   public void periodic() {
     odometry.update(Rotation2d.fromDegrees(getHeading()), leftEncoder.getPosition(), rightEncoder.getPosition());
+  }
+
+  public SimpleSplines.SplineAutoConstants getAutoConstants(){
+    return autoConstants;
+  }
+
+  public SimpleSplines.SplineDriveConstants getDriveConstants(){
+    return driveConstants;
   }
 
   public TankDrive getDriveBase(){
@@ -85,10 +114,10 @@ public class SensorDrive implements Subsystem { // Based largely on https://gith
    */
   public void tankDriveVolts(double leftVolts, double rightVolts) {
     Motor[] motors = driveBase.getMotors();
-    if(motors.length == 2){
+    if (motors.length == 2) {
       driveBase.getMotors()[0].setVoltage(leftVolts);
       driveBase.getMotors()[1].setVoltage(rightVolts);
-    }else{
+    } else {
       driveBase.getMotors()[0].setVoltage(leftVolts);
       driveBase.getMotors()[1].setVoltage(leftVolts);
       driveBase.getMotors()[2].setVoltage(rightVolts);
@@ -103,6 +132,7 @@ public class SensorDrive implements Subsystem { // Based largely on https://gith
     leftEncoder.setPosition(0);
     rightEncoder.setPosition(0);
   }
+
   /**
    * Returns the heading of the robot.
    *
@@ -110,5 +140,24 @@ public class SensorDrive implements Subsystem { // Based largely on https://gith
    */
   public double getHeading() {
     return gyro.getYaw() * -1;
+  }
+
+  public void configuratePath(double maxVoltage){
+    pathConfig = new TrajectoryConfig(autoConstants.kMaxSpeedMetersPerSecond, autoConstants.kMaxAccelerationMetersPerSecondSquared)
+        .setKinematics(driveConstants.kDriveKinematics)
+        .addConstraint(new DifferentialDriveVoltageConstraint(
+          new SimpleMotorFeedforward(driveConstants.ksVolts, 
+          driveConstants.kvVoltSecondsPerMeter, 
+          driveConstants.kaVoltSecondsSquaredPerMeter), 
+          driveConstants.kDriveKinematics, 
+          maxVoltage));
+  }
+
+  public Trajectory generateSimpleTrajectory(Pose2d init_pos, List<Translation2d> inter_points, Pose2d final_pos){
+    return TrajectoryGenerator.generateTrajectory(init_pos, inter_points, final_pos, pathConfig);
+  }
+
+  public Trajectory generateQuinticTrajectory(List<Pose2d> waypoints){
+    return TrajectoryGenerator.generateTrajectory(waypoints, pathConfig);
   }
 }
