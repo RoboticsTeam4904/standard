b701088835d9d441a4f67857b385c59cf1e39821
Billy Pierce <bilpier@nuevaschool.org>
2020-02-10 17:25:17 -0800

    undo command base changes

--- commands/Cancel.java
+++ commands/Cancel.java
@@ -1,17 +1,15 @@
 package org.usfirst.frc4904.standard.commands;
 
 import org.usfirst.frc4904.standard.LogKitten;
-
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /**
  * This class cancels a command
  */
 public class Cancel extends CommandBase {
-	protected final Command command;
+	protected final CommandBase command;
 
-	public Cancel(Command command) {
+	public Cancel(CommandBase command) {
 		super();
 		setName("Cancel " + command.getName());
 		this.command = command;
