0ffed7d4d4f57e1513cceb3231879b01d67c4d12
zbuster05 <zakary.saya@gmail.com>
2022-01-13 17:12:04 -0800

    Marked all tested files

--- custom/sensors/CustomCANCoder.java
+++ custom/sensors/CustomCANCoder.java
@@ -1,3 +1,5 @@
+// THIS FILE IS TESTED post wpilibj2
+
 package org.usfirst.frc4904.standard.custom.sensors;
 
 import com.ctre.phoenix.sensors.CANCoder;
--- custom/sensors/NavX.java
+++ custom/sensors/NavX.java
@@ -1,3 +1,5 @@
+// THIS FILE IS TESTED post wpilibj2
+
 package org.usfirst.frc4904.standard.custom.sensors;
 
 import org.usfirst.frc4904.standard.custom.CustomPIDSourceType;
