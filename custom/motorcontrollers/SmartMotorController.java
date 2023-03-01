957950bbf6a9b4e1b4194928555340f1647be4c2
roboticsteam4904-2 <102199775+roboticsteam4904-2@users.noreply.github.com>
2023-02-28 20:12:16 -0800

    make smartMotorController extend WPILIB::MotorController Co-authored-by: Exr0n <mail@exr0n.com>

--- custom/motorcontrollers/SmartMotorController.java
+++ custom/motorcontrollers/SmartMotorController.java
@@ -3,15 +3,15 @@
 
 package org.usfirst.frc4904.standard.custom.motorcontrollers;
 
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+
 /**
  * Represents a "smart" motor controller, like the TalonFX, TalonSRX, or SparkMax
  * 
  * These should support brake mode, follow mode, limit switches, and various closed-loop control modes.
  */
-public interface SmartMotorController {
-	public abstract void set(double power);
+public interface SmartMotorController extends MotorController {
 	public abstract void setPower(double power);
-	public abstract void setVoltage(double voltage);
 
 	public abstract SmartMotorController setBrakeOnNeutral();
 	public abstract SmartMotorController setCoastOnNeutral();
