de513fedd68842acf7795f1ca910ab094f7ac3ea
roboticsteam4904-2 <botprovoking@gmail.com>
2023-03-12 14:56:54 -0700

    remove brakeablemotorcontroller

--- custom/motorcontrollers/CustomCANSparkMax.java
+++ custom/motorcontrollers/CustomCANSparkMax.java
@@ -2,17 +2,9 @@ package org.usfirst.frc4904.standard.custom.motorcontrollers;
 
 import com.revrobotics.CANSparkMax;
 
-<<<<<<< HEAD
-import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
 
-public class CustomCANSparkMax extends CANSparkMax implements BrakeableMotorController {
-    protected static final NeutralMode DEFAULT_NEUTRAL_MODE = NeutralMode.Coast;
-	protected static final InvertType  DEFAULT_INVERT_TYPE  = InvertType.FollowMaster;
-    protected Double saturation_voltage = null;   // remember the configured saturation voltage to conform to the talon api of having separate config() and enable() methods; error if we try to enable without configuring it.
-=======
 public class CustomCANSparkMax extends CANSparkMax implements SmartMotorController {
     // protected Double voltage_compensation_max = null;   // remember the configured saturation voltage to conform to the talon api of having separate config() and enable() methods; error if we try to enable without configuring it.
->>>>>>> 3cd05009846892b079be825789d18e63437b3f73
     
     public CustomCANSparkMax(int deviceNumber, MotorType motorType, boolean inverted) {
         super(deviceNumber, motorType);
