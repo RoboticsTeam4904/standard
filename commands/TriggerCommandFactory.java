d2415f03fcdc8e09e11719fc5cfef17850fc6de0
Exr0n <mail@exr0n.com>
2023-04-03 14:40:14 -0700

    named triggerCommandFactory

--- commands/TriggerCommandFactory.java
+++ commands/TriggerCommandFactory.java
@@ -9,7 +9,15 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 public class TriggerCommandFactory extends CommandBase {
     private final Supplier<Command> commandDealer;
     private Command currentActiveCommand = null;
+    private final String name;
     
+    public TriggerCommandFactory(String name, Supplier<Command> commandDealer) {
+        this.commandDealer = commandDealer;
+        this.name = name;
+        setName(name);
+        // TODO: do we need to add requirements?
+        // var reqs = this.commandDealer.get().getRequirements(); addRequirements(reqs.toArray(new Subsystem[reqs.size()]));
+    }
     /**
      * Command that takes in a command factory and runs a new command from that
      * factory each time it is scheduled. This avoids the problems of command
@@ -18,9 +26,7 @@ public class TriggerCommandFactory extends CommandBase {
      * @param commandDealer The Command factory
      */
     public TriggerCommandFactory(Supplier<Command> commandDealer) {
-        this.commandDealer = commandDealer;
-        // TODO: do we need to add requirements?
-        // var reqs = this.commandDealer.get().getRequirements(); addRequirements(reqs.toArray(new Subsystem[reqs.size()]));
+        this("Unnamed TriggerCommandFactory", commandDealer);
     }
     @Override
     public void initialize() {
