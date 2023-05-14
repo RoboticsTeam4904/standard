d2218bf5cbde0224df29fd055acdabff2c3ec14c
Exr0n <mail@exr0n.com>
2023-05-13 20:39:22 -0700

    clean up CreateAndDisown

--- commands/CreateAndDisown.java
+++ commands/CreateAndDisown.java
@@ -4,18 +4,16 @@ import java.util.function.Supplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 
+// to be replaced by CommandBased
 public class CreateAndDisown extends CommandBase {
     private final Supplier<Command> commandDealer;
     private Command currentActiveCommand = null;
-    private final String name;
     
     public CreateAndDisown(String name, Supplier<Command> commandDealer) {
         this.commandDealer = commandDealer;
-        this.name = name;
         setName(name);
-        // TODO: do we need to add requirements?
+        // do we need to add requirements? probably not, this ends immediately anyway
         // var reqs = this.commandDealer.get().getRequirements(); addRequirements(reqs.toArray(new Subsystem[reqs.size()]));
     }
     /**
@@ -26,13 +24,13 @@ public class CreateAndDisown extends CommandBase {
      * @param commandDealer The Command factory
      */
     public CreateAndDisown(Supplier<Command> commandDealer) {
-        this("Unnamed TriggerCommandFactory", commandDealer);
+        this("Unnamed CreateAndDisown", commandDealer);
     }
     @Override
     public void initialize() {
         currentActiveCommand = commandDealer.get();
         currentActiveCommand.schedule();
-        setName("trigger: " + currentActiveCommand.getName());
+        setName("C&D: " + currentActiveCommand.getName());
     }
     @Override
     public void execute() {
