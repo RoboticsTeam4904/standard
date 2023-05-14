ef7dc7c7bd4867dc5b91a9b4fa42c89bc7088a7a
zbuster05 <zakary.saya@gmail.com>
2023-05-13 22:39:22 -0500

    can end

--- custom/motioncontrollers/ezMotion.java
+++ custom/motioncontrollers/ezMotion.java
@@ -1,13 +1,16 @@
 package org.usfirst.frc4904.standard.custom.motioncontrollers;
 
 import java.util.function.DoubleConsumer;
-import java.util.function.DoubleFunction;
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
+import org.usfirst.frc4904.standard.custom.motioncontrollers.ezMotion.SetpointProvider.EndSignal;
+
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 public class ezMotion extends CommandBase {
@@ -15,18 +18,33 @@ public class ezMotion extends CommandBase {
     public DoubleConsumer processVariable;
     public Double initialTimestamp;
     public DoubleSupplier feedback;
-    public Supplier<DoubleFunction<Pair<Double, Double>>> setpointDealerDealer;
-    public DoubleFunction<Pair<Double, Double>> setpointDealer = null;
 
-    public ezMotion(ezControl control, DoubleSupplier feedback, DoubleConsumer processVariable, Supplier<DoubleFunction<Pair<Double, Double>>> setpointDealerDealer, Subsystem... requirements) {  // FIXME: use Pair<Double, Double>
+    private double setpoint;
+    private double setpoint_dt;
+
+    public Supplier<SetpointProvider<Pair<Double, Double>>> setpointDealerDealer;
+    public SetpointProvider<Pair<Double, Double>> setpointDealer = null;
+
+    public Command onArrival;
+    
+    public ezMotion(ezControl control, DoubleSupplier feedback, DoubleConsumer processVariable, Supplier<SetpointProvider<Pair<Double, Double>>> setpointDealerDealer, Command onArrival, Subsystem... requirements) {
         addRequirements(requirements);
         this.control = control;
         this.processVariable = processVariable;
         this.feedback = feedback;
         this.setpointDealerDealer = setpointDealerDealer;
+        this.onArrival = onArrival != null? (onArrival) : (new InstantCommand(() -> {}));
     }
 
-    public ezMotion(ezControl control, DoubleSupplier feedback, DoubleConsumer processVariable, DoubleFunction<Pair<Double, Double>> setpointDealer, Subsystem... requirements) {
+    public ezMotion(ezControl control, DoubleSupplier feedback, DoubleConsumer processVariable, Supplier<SetpointProvider<Pair<Double, Double>>> setpointDealerDealer, Subsystem... requirements) {
+        this(control, feedback, processVariable, setpointDealerDealer, new InstantCommand(() -> {}), requirements);
+    }
+
+    public ezMotion(ezControl control, DoubleSupplier feedback, DoubleConsumer processVariable, SetpointProvider<Pair<Double, Double>> setpointDealer, Command onArrival, Subsystem... requirements) {
+        this(control, feedback, processVariable, () -> setpointDealer, onArrival, requirements);
+    }
+
+    public ezMotion(ezControl control, DoubleSupplier feedback, DoubleConsumer processVariable, SetpointProvider<Pair<Double, Double>> setpointDealer, Subsystem... requirements) {
         this(control, feedback, processVariable, () -> setpointDealer, requirements);
     }
 
@@ -46,9 +64,15 @@ public class ezMotion extends CommandBase {
 
     @Override
     public void execute() {
-        Pair<Double, Double> setpoints = this.setpointDealer.apply(getElapsedTime());
-        double setpoint = setpoints.getFirst();
-        double setpoint_dt = setpoints.getSecond();
+        Pair<Double, Double> setpoints;
+        try {
+            setpoints = this.setpointDealer.apply(getElapsedTime());
+            setpoint = setpoints.getFirst();
+            setpoint_dt = setpoints.getSecond();
+
+        } catch (EndSignal e) {
+            onArrival.schedule();
+        }
 
         control.updateSetpoint(setpoint, setpoint_dt);
         double controlEffort = control.calculate(feedback.getAsDouble(), getElapsedTime());
@@ -56,13 +80,13 @@ public class ezMotion extends CommandBase {
     }
 
     @Override
-    public boolean isFinished() {
-        return false;
-    }
+    public boolean isFinished() { return false; }
 
-    @Override
-    public void end(boolean interrupted) {
-        // FIXME Auto-generated method stub
-        super.end(interrupted);
+    @FunctionalInterface
+    public interface SetpointProvider<R> {
+        public class EndSignal extends Throwable {}
+        
+        public R apply(double num) throws EndSignal;
     }
+    
 }
