07137893126f24904b579a0f5445e7fbf6b5a310
Robby20914 <sofia.robison@gmail.com>
2025-02-12 17:14:00 -0800

    Slightly unbroke sparkmax motor controllers section  added comment abt why broke

--- custom/motorcontrollers/CustomCANSparkMax.java
+++ custom/motorcontrollers/CustomCANSparkMax.java
@@ -1,55 +1,61 @@
-// package org.usfirst.frc4904.standard.custom.motorcontrollers;
+package org.usfirst.frc4904.standard.custom.motorcontrollers;
 
-// import com.revrobotics.spark.SparkMax;
-// import com.revrobotics.spark.config.LimitSwitchConfig.Type;
-// import com.revrobotics.spark.SparkLimitSwitch;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.LimitSwitchConfig.Type;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkLimitSwitch;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+public class CustomCANSparkMax extends SparkMax implements SmartMotorController {
+    // protected Double voltage_compensation_max = null;   // remember the configured saturation voltage to conform to the talon api of having separate config() and enable() methods; error if we try to enable without configuring it.
+    protected Type switchType;
 
-// public class CustomCANSparkMax extends SparkMax implements SmartMotorController {
-//     // protected Double voltage_compensation_max = null;   // remember the configured saturation voltage to conform to the talon api of having separate config() and enable() methods; error if we try to enable without configuring it.
-//     protected Type switchType;
-
-//     public CustomCANSparkMax(int deviceNumber, MotorType motorType, boolean inverted, Type limitSwitchType) {
-//         super(deviceNumber, motorType);
-//         setInverted(inverted);
-//         this.switchType = limitSwitchType;  // have to store it bc we need to error if we try to read switch pressed state without knowing switch type
-//         super.restoreFactoryDefaults();
-//     }
+    public CustomCANSparkMax(int deviceNumber, MotorType motorType, boolean inverted, Type limitSwitchType) {
+        super(deviceNumber, motorType);
+        setInverted(inverted);
+        this.switchType = limitSwitchType;  // have to store it bc we need to error if we try to read switch pressed state without knowing switch type
+        super.restoreFactoryDefaults();
+    }
     
-//     public CustomCANSparkMax(int deviceNumber, MotorType motorType, boolean inverted) {
-//         this(deviceNumber, motorType, inverted, null);
-//     }
+    public CustomCANSparkMax(int deviceNumber, MotorType motorType, boolean inverted) {
+        this(deviceNumber, motorType, inverted, null);
+    }
 
-//     /**
-//      * Alias for .set() on power
-//      * @param power
-//      */
-//     public void setPower(double power) { set(power); }
+    /**
+     * Alias for .set() on power
+     * @param power
+     */
+    public void setPower(double power) { set(power); }
 
-//     public SmartMotorController setBrakeOnNeutral() {
-//         setIdleMode(IdleMode.kBrake);
-//         return this;
-//     }
+    
 
-// 	public SmartMotorController setCoastOnNeutral() {
-//         setIdleMode(IdleMode.kCoast);
-//         return this;
-//     }
+    @Override
+    public void neutralOutput() {
+        stopMotor(); 
+    }
 
-//     @Override
-//     public void neutralOutput() {
-//         stopMotor(); 
-//     }
+    @Override
+    public boolean isFwdLimitSwitchPressed() throws IllegalAccessException {
+        if (switchType == null) throw new IllegalAccessException("Cannot read limit switch state when CustomCANSparkMax was constructed without limit switch type!");
+        return getForwardLimitSwitch().isPressed();
+    }
 
-//     @Override
-//     public boolean isFwdLimitSwitchPressed() throws IllegalAccessException {
-//         if (switchType == null) throw new IllegalAccessException("Cannot read limit switch state when CustomCANSparkMax was constructed without limit switch type!");
-//         return getForwardLimitSwitch(switchType).isPressed();
-//     }
+    @Override
+    public boolean isRevLimitSwitchPressed() throws IllegalAccessException {
+        if (switchType == null) throw new IllegalAccessException("Cannot read limit switch state when CustomCANSparkMax was constructed without limit switch type!");
+        return getReverseLimitSwitch().isPressed();
+    }
 
-//     @Override
-//     public boolean isRevLimitSwitchPressed() throws IllegalAccessException {
-//         if (switchType == null) throw new IllegalAccessException("Cannot read limit switch state when CustomCANSparkMax was constructed without limit switch type!");
-//         return getReverseLimitSwitch(switchType).isPressed();
-//     }
-// }
+    @Override
+    public SmartMotorController setBrakeOnNeutral() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setBrakeOnNeutral'");
+    }
+
+    @Override
+    public SmartMotorController setCoastOnNeutral() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setCoastOnNeutral'");
+    }
+}
