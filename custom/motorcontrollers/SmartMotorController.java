9315c1eb8158dde822c976347cf30a5245edaba3
Sam Matteson <sammatt@nuevaschool.org>
2025-02-13 17:22:08 -0800

    remove unnecessary modifier keyword things whatever theyre called

--- custom/motorcontrollers/SmartMotorController.java
+++ custom/motorcontrollers/SmartMotorController.java
@@ -11,12 +11,12 @@ import edu.wpi.first.wpilibj.motorcontrol.MotorController;
  * These should support brake mode, follow mode, limit switches, and various closed-loop control modes.
  */
 public interface SmartMotorController extends MotorController {
-	public abstract void setPower(double power);
+	void setPower(double power);
 
-	public abstract boolean isFwdLimitSwitchPressed() throws IllegalAccessException;	// limit switch methods are here because: talonfx and talonsrx has different types for sensorcollection and I didn't want to make TalonMotorSubsystem generic -> require a type parameter; sparkmax also has it. can't be on subsystem level (because talonMotorSubsystem doesn't differentiate b/w talonfx and talonsrx) so it has to be here.
-	public abstract boolean isRevLimitSwitchPressed() throws IllegalAccessException;	// needs to throw an exception so that the downstream CustomCANSparkMax can throw an exception if we try to read from limit switches before they are declared as normallyOpen or normallyClosed
+	boolean isFwdLimitSwitchPressed() throws IllegalAccessException;	// limit switch methods are here because: talonfx and talonsrx has different types for sensorcollection and I didn't want to make TalonMotorSubsystem generic -> require a type parameter; sparkmax also has it. can't be on subsystem level (because talonMotorSubsystem doesn't differentiate b/w talonfx and talonsrx) so it has to be here.
+	boolean isRevLimitSwitchPressed() throws IllegalAccessException;	// needs to throw an exception so that the downstream CustomCANSparkMax can throw an exception if we try to read from limit switches before they are declared as normallyOpen or normallyClosed
 
-	public abstract SmartMotorController setBrakeOnNeutral();
-	public abstract SmartMotorController setCoastOnNeutral();
-	public abstract void neutralOutput();
+	SmartMotorController setBrakeOnNeutral();
+	SmartMotorController setCoastOnNeutral();
+	void neutralOutput();
 }
